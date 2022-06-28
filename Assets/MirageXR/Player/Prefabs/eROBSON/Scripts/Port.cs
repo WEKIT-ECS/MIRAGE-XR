98fa434a34ba02fe91afce9cb9cdb803c87dc91b
Abbas Jafari <a85jafari@gmail.com>
2022-06-28 16:10:34 +0200

    Snapping functions are refactored

--- Assets/MirageXR/Player/Prefabs/eROBSON/Scripts/Port.cs
+++ Assets/MirageXR/Player/Prefabs/eROBSON/Scripts/Port.cs
@@ -25,13 +25,14 @@ public class Port : MonoBehaviour
 
     private eROBSONItems detectedBit;
 
-    private float bitScaleFactor = 0.23f;
-
     public bool Connected { get; private set; }
+
+    private Rigidbody rb;
 
     private void Start()
     {
         myBit = transform.parent.gameObject;
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -59,21 +60,36 @@ public class Port : MonoBehaviour
         //If a port with different charge is detected
         if (detectedPortPole && detectedPortPole.Pole != Pole.NEUTRAL && touchedObject != null)
         {
-            //Move the bit to the left or right side of this bit depends on the port
-            var connectionPosition = Pole == Pole.POSITIVE ? detectedPortPole.transform.forward : -detectedPortPole.transform.forward;
 
             //if the detected port has NOT the same charge (different charge)
             if (detectedPortPole.Pole != Pole)
             {
+                //Make the pool be the parent of the bit
+                foreach (var child in myBit.GetComponentsInChildren<Transform>())
+                {
+                    if(child.parent == myBit.transform)
+                    {
+                        child.SetParent(transform);
+                    }
+                }
+
                 //Snap the bit to the port
                 detectedBit.DisableManipulation();
-                myBit.transform.position = detectedPortPole.transform.position  + new Vector3(0, 0, bitScaleFactor);
+
+                //Move the bit to the pole of the other bit (Snapping)
+                rb.MovePosition(detectedPortPole.transform.position);
                 Connected = true;
             }
             else
             {
+                //Move the bit to the left or right side of this bit depends on the port
+                var connectionPosition = Pole == Pole.POSITIVE ? detectedPortPole.transform.forward : -detectedPortPole.transform.forward;
+
                 //Make a distance from the detected port
-                myBit.transform.position = detectedPortPole.transform.position + connectionPosition * bitScaleFactor * 1.5f;
+                transform.position = detectedPortPole.transform.position + connectionPosition * 0.2f;
+
+                //Make the bit item as the bit parent again
+                ReParentComponent();
             }
             
         }
@@ -81,6 +97,9 @@ public class Port : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+
+        //Make the bit item as the bit parent again
+        ReParentComponent();
         Connected = false;
         detectedPortPole = null;
 
@@ -89,6 +108,21 @@ public class Port : MonoBehaviour
             detectedBit.EnableManipulation();
             detectedBit = null;
         }
+    }
+
+    private void ReParentComponent()
+    {
+        gameObject.transform.SetParent(myBit.transform);
+
+        foreach (var child in transform.GetComponentsInChildren<Transform>())
+        {
+            if (child.parent == transform)
+            {
+                child.SetParent(myBit.transform);
+            }
+        }
+
+       
     }
 
 }
