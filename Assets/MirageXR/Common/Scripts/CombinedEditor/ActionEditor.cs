4abd663489c61a13d42f91e9965361399d4be3f7
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2022-06-30 15:15:40 +0200

    fixed SA1622: added documentation for generic parameter where it was previously empty

--- Assets/MirageXR/Common/Scripts/CombinedEditor/ActionEditor.cs
+++ Assets/MirageXR/Common/Scripts/CombinedEditor/ActionEditor.cs
@@ -404,7 +404,7 @@ public class ActionEditor : MonoBehaviour
     /// This function instantiates the relevant prefab and, depending on the augmentation type,
     /// allows the controller component to be returned after configuration.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The component which should be searched on the instantiated prefab and which is returned</typeparam>
     /// <param name="prefabToLoad"></param>
     /// <returns>A component of the type specified during the function call.</returns>
     private T LoadEditorPanel<T>(GameObject prefabToLoad) where T : Component
