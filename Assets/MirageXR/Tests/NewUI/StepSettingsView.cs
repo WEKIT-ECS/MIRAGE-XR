bbbd6f7dc5f59a444ebeff2e3fc740738465ee64
Alvareon <sergeyilyasov@gmail.com>
2022-06-30 03:40:49 +0300

    added StepSettings popup

---
+++ Assets/MirageXR/Tests/NewUI/StepSettingsView.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StepSettingsView : PopupBase
+{
+    [SerializeField] private Button _btnClose;
+    [SerializeField] private Button _btnCopyStep;
+    [SerializeField] private Button _btnDeleteStep;
+
+    public override void Init(Action<PopupBase> onClose, params object[] args)
+    {
+        base.Init(onClose, args);
+        _btnClose.onClick.AddListener(Close);
+        _btnCopyStep.onClick.AddListener(CopyStep);
+        _btnDeleteStep.onClick.AddListener(DeleteStep);
+    }
+
+    protected override bool TryToGetArguments(params object[] args)
+    {
+        return true;
+    }
+
+    private void CopyStep()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DeleteStep()
+    {
+        throw new NotImplementedException();
+    }
+}
