54deb1284d8ff851bdbce74ec4661704446ee91a
Alvareon <sergeyilyasov@gmail.com>
2022-07-14 00:05:18 +0300

    added ProfileView

---
+++ Assets/MirageXR/Tests/NewUI/ProfileView.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileView : PopupBase
+{
+    protected override bool TryToGetArguments(params object[] args)
+    {
+        return true;
+    }
+}
