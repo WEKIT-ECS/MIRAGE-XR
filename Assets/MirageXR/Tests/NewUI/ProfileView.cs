88fbaaa0c4ac6b0af0a7cc6011d56207d1bace58
Alvareon <sergeyilyasov@gmail.com>
2022-07-14 04:31:19 +0300

    fix transitions

--- Assets/MirageXR/Tests/NewUI/ProfileView.cs
+++ Assets/MirageXR/Tests/NewUI/ProfileView.cs
@@ -1,11 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProfileView : PopupBase
 {
+    [SerializeField] private MoodleServersView _moodlePrefab;
+    [SerializeField] private Button _selectServer;
     protected override bool TryToGetArguments(params object[] args)
     {
         return true;
+    }
+    
+    public override void Init(Action<PopupBase> onClose, params object[] args)
+    {
+        base.Init(onClose, args);
+        
+        _selectServer.onClick.AddListener(ShowServerPanel);
+    }
+
+    private void ShowServerPanel()
+    {
+        PopupsViewer.Instance.Show(_moodlePrefab);
     }
 }
