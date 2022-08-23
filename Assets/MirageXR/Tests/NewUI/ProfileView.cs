47467c57d068b8d68833b368fe5df32af8b22bf4
robhillman97 <robhillman97@gmail.com>
2022-08-23 10:56:24 +0100

    cleaned up profile scripts and prefabs

--- Assets/MirageXR/Tests/NewUI/ProfileView.cs
+++ Assets/MirageXR/Tests/NewUI/ProfileView.cs
@@ -180,8 +180,6 @@ public class ProfileView : PopupBase
 
     private void UpdateConectedLRS(int publicCurrentLearningRecordStore)
     {
-
-        Debug.Log("LRS ==== " + publicCurrentLearningRecordStore);
         switch (publicCurrentLearningRecordStore)
         {
             case 0:
--- Assets/MirageXR/Tests/NewUI/SelectLRS.cs
+++ Assets/MirageXR/Tests/NewUI/SelectLRS.cs
@@ -41,4 +41,6 @@ public class SelectLRS : PopupBase
         Close();
     }
 
+
+
 }
