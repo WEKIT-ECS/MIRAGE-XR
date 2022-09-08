b2ea6e737875dfb9fa6e91e6d33c05fed0759814
Alex Shubin <icawke@gmail.com>
2022-09-08 03:11:14 +0300

    dialog has been updated

--- Assets/MirageXR/Player/Scripts/Mobile/DialogWindow/DialogViewBottomMultiline.cs
+++ Assets/MirageXR/Player/Scripts/Mobile/DialogWindow/DialogViewBottomMultiline.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogViewBottom : DialogView
+public class DialogViewBottomMultiline : DialogView
 {
     [SerializeField] private Button _buttonClose;
     [SerializeField] private Button _buttonPrefab;
