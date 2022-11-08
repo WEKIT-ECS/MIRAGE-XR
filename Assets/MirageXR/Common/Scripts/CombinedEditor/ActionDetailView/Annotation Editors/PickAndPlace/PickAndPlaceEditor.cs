e1c6d54da9ce3c639e84b0379abf1164ccedbfe4
robhillman97 <robhillman97@gmail.com>
2022-11-08 13:53:04 +0000

    Adds triggers to pick and place and adds trigger settings menu on hololens UI

--- Assets/MirageXR/Common/Scripts/CombinedEditor/ActionDetailView/Annotation Editors/PickAndPlace/PickAndPlaceEditor.cs
+++ Assets/MirageXR/Common/Scripts/CombinedEditor/ActionDetailView/Annotation Editors/PickAndPlace/PickAndPlaceEditor.cs
@@ -5,12 +5,29 @@ using UnityEngine.UI;
 public class PickAndPlaceEditor : MonoBehaviour
 {
     private static AugmentationManager augmentationManager => RootObject.Instance.augmentationManager;
+    private static ActivityManager activityManager => RootObject.Instance.activityManager;
     private static WorkplaceManager workplaceManager => RootObject.Instance.workplaceManager;
 
     [SerializeField] private Transform annotationStartingPoint;
     [SerializeField] private InputField textInputField;
 
+    [SerializeField] private Toggle toggleTrigger;
+    [SerializeField] private InputField triggerStepIndex;
+    [SerializeField] private InputField triggerStepTime;
+    [SerializeField] private GameObject triggerHelp;
+    [SerializeField] private GameObject triggerIndexHelp;
+    [SerializeField] private GameObject triggerTimeHelp;
 
+    [SerializeField] private GameObject editor;
+    [SerializeField] private GameObject triggerSettings;
+
+    [SerializeField] private Text hoverGuide;
+
+    private const string TriggerHelpText = "This toggle gives this pick and place augmentation a trigger, allowing you to jump to another step once placed correctly";
+    private const string TriggerIndexHelpText = "Use this input box to enter the step the trigger should take you to";
+    private const string TriggerTimeHelpText = "use this input box to determine the time between correct placement and moving onto the set step";
+
+    private bool isTrigger;
     private Action action;
     private ToggleObject annotationToEdit;
     private int resetOption = 0;
@@ -43,6 +60,15 @@ public class PickAndPlaceEditor : MonoBehaviour
         annotationToEdit.text = textInputField.text;
         annotationToEdit.key = resetOption.ToString();
 
+        if (isTrigger)
+        {
+            action.AddOrReplaceArlemTrigger(TriggerMode.PickAndPlace, ActionType.PickAndPlace, annotationToEdit.poi, int.Parse(triggerStepTime.text), triggerStepIndex.text);
+        }
+        else
+        {
+            action.RemoveArlemTrigger(annotationToEdit);
+        }
+
         EventManager.ActivateObject(annotationToEdit);
         EventManager.NotifyActionModified(action);
 
@@ -61,9 +87,27 @@ public class PickAndPlaceEditor : MonoBehaviour
     public void Open(Action action, ToggleObject annotation)
     {
         gameObject.SetActive(true);
+        CloseTriggerSettings();
         this.action = action;
         annotationToEdit = annotation;
         textInputField.text = annotation != null ? annotation.text : string.Empty;
+        isTrigger = false;
+
+        AddHoverGuide(triggerHelp, TriggerHelpText);
+        AddHoverGuide(triggerIndexHelp, TriggerIndexHelpText);
+        AddHoverGuide(triggerTimeHelp, TriggerTimeHelpText);
+
+        if (annotationToEdit != null)
+        {
+            var trigger = activityManager.ActiveAction.triggers.Find(t => t.id == annotationToEdit.poi);
+            isTrigger = trigger != null ? true : false;
+            if (isTrigger)
+            {
+                toggleTrigger.isOn = isTrigger;
+                triggerStepTime.text = trigger.duration.ToString();
+                triggerStepIndex.text = trigger.value;
+            }
+        }
     }
 
     public void setResetOption(int option)
@@ -71,4 +115,65 @@ public class PickAndPlaceEditor : MonoBehaviour
         resetOption = option;
     }
 
+    public void triggerToggle(bool trigger)
+    {
+        var numberOfSteps = activityManager.ActionsOfTypeAction.Count;
+
+        if (numberOfSteps == 1)
+        {
+            if (toggleTrigger.isOn)
+            {
+                DialogWindow.Instance.Show(
+                "Info!",
+                "Only one step has been found in this activity!\n Add a new step and try again.",
+                new DialogButtonContent("Ok"));
+
+                toggleTrigger.isOn = false;
+            }
+            return;
+        }
+        else
+        {
+            isTrigger = toggleTrigger.isOn;
+            triggerStepIndex.interactable = toggleTrigger.isOn;
+            triggerStepIndex.text = numberOfSteps.ToString();
+            triggerStepTime.interactable = toggleTrigger.isOn;
+            triggerStepTime.text = "1";
+        }
+    }
+
+    public void OpenTriggerSettings()
+    {
+        editor.SetActive(false);
+        triggerSettings.SetActive(true);
+    }
+
+    public void CloseTriggerSettings()
+    {
+        editor.SetActive(true);
+        triggerSettings.SetActive(false);
+    }
+
+    public void OnStepTriggerValueChanged()
+    {
+        var numberOfSteps = activityManager.ActionsOfTypeAction.Count;
+
+        if (numberOfSteps < int.Parse(triggerStepIndex.text))
+        {
+            DialogWindow.Instance.Show(
+            "Info!",
+            "The entered step number doesn't exist yet. This trigger will jump to the last avalible step",
+            new DialogButtonContent("Ok"));
+
+            triggerStepIndex.text = numberOfSteps.ToString();
+            return;
+        }
+    }
+
+    private void AddHoverGuide(GameObject obj, string hoverMessage)
+    {
+        var HoverGuilde = obj.AddComponent<HoverGuilde>();
+        HoverGuilde.SetGuildText(hoverGuide);
+        HoverGuilde.SetMessage(hoverMessage);
+    }
 }
