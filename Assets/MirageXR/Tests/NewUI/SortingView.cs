9d160999263e3d414e7a18f506f63f3d928a2b3f
robhillman97 <robhillman97@gmail.com>
2022-12-16 12:01:23 +0000

    Added sort by and show by functionality to sorting view

--- Assets/MirageXR/Tests/NewUI/SortingView.cs
+++ Assets/MirageXR/Tests/NewUI/SortingView.cs
@@ -1,5 +1,5 @@
-using System;
 using MirageXR;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +9,12 @@ public class SortingView : PopupBase
     [SerializeField] private Toggle _toggleSmallCards;
     [SerializeField] private Toggle _toggleBigCards;
 
+    [SerializeField] private Toggle _toggleShowAll;
+    [SerializeField] private Toggle _toggleMyAssignments;
+    [SerializeField] private Toggle _toggleMyActivities;
+    [SerializeField] private Toggle _toggleByDate;
+    [SerializeField] private Toggle _toggleByRelevance;
+
     private ActivityListView_v2 _parentView;
 
     public override void Initialization(Action<PopupBase> onClose, params object[] args)
@@ -17,19 +23,57 @@ public class SortingView : PopupBase
 
         _btnClose.onClick.AddListener(Close);
 
-        if (DBManager.showBigCards)
-        {
-            _toggleSmallCards.isOn = false;
-            _toggleBigCards.isOn = true;
-        }
-        else
-        {
-            _toggleBigCards.isOn = false;
-            _toggleSmallCards.isOn = true;
-        }
+        UpdateView();
+
+        _toggleShowAll.onValueChanged.AddListener(ShowAll);
+        _toggleMyAssignments.onValueChanged.AddListener(ShowMyAssignments);
+        _toggleMyActivities.onValueChanged.AddListener(ShowMyActivities);
+        _toggleByDate.onValueChanged.AddListener(SortByDate);
+        _toggleByRelevance.onValueChanged.AddListener(SortByRelevance);
+
 
         _toggleSmallCards.onValueChanged.AddListener(ShowSmallCard);
         _toggleBigCards.onValueChanged.AddListener(ShowBigCard);
+    }
+
+    private void UpdateView()
+    {
+        _toggleSmallCards.isOn = !DBManager.showBigCards;
+        _toggleBigCards.isOn = DBManager.showBigCards;
+
+        switch (DBManager.currentShowby)
+        {
+            case DBManager.ShowBy.ALL:
+                _toggleShowAll.isOn = true;
+                _toggleMyAssignments.isOn = false;
+                _toggleMyActivities.isOn = false;
+                break;
+            case DBManager.ShowBy.MYACTIVITIES:
+                _toggleShowAll.isOn = false;
+                _toggleMyActivities.isOn = true;
+                _toggleMyAssignments.isOn = false;
+                break;
+            case DBManager.ShowBy.MYASSIGNMENTS:
+                _toggleShowAll.isOn = false;
+                _toggleMyAssignments.isOn = true;
+                _toggleMyActivities.isOn = false;
+                break;
+        }
+
+        switch (DBManager.currentSortby)
+        {
+            case DBManager.SortBy.DATE:
+                _toggleByRelevance.isOn = false;
+                _toggleByDate.isOn = true;
+
+                break;
+            case DBManager.SortBy.RELAVEANCE:
+                _toggleByRelevance.isOn = true;
+                _toggleByDate.isOn = false;
+
+                break;
+        }
+
     }
 
     protected override bool TryToGetArguments(params object[] args)
@@ -56,5 +100,49 @@ public class SortingView : PopupBase
         DBManager.showBigCards = true;
         _parentView.UpdateView();
     }
+
+    private void ShowAll(bool value)
+    {
+        DBManager.currentShowby = DBManager.ShowBy.ALL;
+        _toggleMyAssignments.isOn = false;
+        _toggleMyActivities.isOn = false;
+
+        _parentView.OnShowbyChanged();
+    }
+
+    private void ShowMyAssignments(bool value)
+    {
+        DBManager.currentShowby = DBManager.ShowBy.MYASSIGNMENTS;
+        _toggleShowAll.isOn = false;
+        _toggleMyActivities.isOn = false;
+
+        _parentView.OnShowbyChanged();
+    }
+
+    private void ShowMyActivities(bool value)
+    {
+        DBManager.currentShowby = DBManager.ShowBy.MYACTIVITIES;
+        _toggleShowAll.isOn = false;
+        _toggleMyAssignments.isOn = false;
+
+        _parentView.OnShowbyChanged();
+    }
+
+    private void SortByDate(bool value)
+    {
+        DBManager.currentSortby = DBManager.SortBy.DATE;
+        _toggleByRelevance.isOn = false;
+
+        _parentView.OnSortbyChanged();
+    }
+
+    private void SortByRelevance(bool value)
+    {
+        DBManager.currentSortby = DBManager.SortBy.RELAVEANCE;
+        _toggleByDate.isOn = false;
+
+        _parentView.OnSortbyChanged();
+    }
+
 
 }
