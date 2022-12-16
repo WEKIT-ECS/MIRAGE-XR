836e1102b38a702a767f8f0b39a91824c2b4daf8
Boris Jovanović <bokisjov@gmail.com>
2022-12-16 10:48:33 +0100

    moved editing tutorial with onboarding

--- Assets/MirageXR/Tests/NewUI/OnboardingView.cs
+++ Assets/MirageXR/Tests/NewUI/OnboardingView.cs
@@ -2,195 +2,195 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class OnboardingView : UIBehaviour, ICanvasElement, IBeginDragHandler, IEndDragHandler, IDragHandler, ILayoutGroup
+namespace MirageXR
 {
-    public GameObject prefab;
-    [SerializeField]
-    protected RectTransform viewport;
-    [SerializeField]
-    protected RectTransform content;
-
-    [SerializeField]
-    private float snapTime = 0.3f;
-    public bool swipeEnabled;
-
-    private bool IsDragging { get; set; } //test
-
-    private float _snapPosition;
-
-    private float _velocity;
-
-    private float _prevPosition;
-
-    private float _viewportWidth;
-    private float _contentWidth;
-
-    private DrivenRectTransformTracker _drivenRectTracker;
-
-    private float ScrollPosition
+    public class OnboardingView : UIBehaviour, ICanvasElement, IBeginDragHandler, IEndDragHandler, IDragHandler, ILayoutGroup
     {
-        get => -content.anchoredPosition.x;
-        set
+        public GameObject prefab;
+        [SerializeField]
+        protected RectTransform viewport;
+        [SerializeField]
+        protected RectTransform content;
+
+        [SerializeField]
+        private float snapTime = 0.3f;
+        public bool swipeEnabled;
+
+        private bool IsDragging { get; set; } //test
+
+        private float _snapPosition;
+
+        private float _velocity;
+
+        private float _prevPosition;
+
+        private float _viewportWidth;
+        private float _contentWidth;
+
+        private DrivenRectTransformTracker _drivenRectTracker;
+
+        private float ScrollPosition
         {
-            var pos = content.anchoredPosition;
-            pos.x = Mathf.Clamp(-value, -content.rect.size.x + viewport.rect.size.x, 0);
-            content.anchoredPosition = pos;
+            get => -content.anchoredPosition.x;
+            set
+            {
+                var pos = content.anchoredPosition;
+                pos.x = Mathf.Clamp(-value, -content.rect.size.x + viewport.rect.size.x, 0);
+                content.anchoredPosition = pos;
+            }
         }
-    }
-    /// <summary>
-    /// The number of tabs.
-    /// </summary>
-    private int Count => content.childCount;
+        /// <summary>
+        /// The number of tabs.
+        /// </summary>
+        private int Count => content.childCount;
 
-    private int _selectedTabIndex = 0;
+        private int _selectedTabIndex = 0;
 
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-        CanvasUpdateRegistry.RegisterCanvasElementForLayoutRebuild(this);
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-        CanvasUpdateRegistry.UnRegisterCanvasElementForRebuild(this);
-    }
-
-    public virtual void Rebuild(CanvasUpdate executing)
-    {
-        if (executing == CanvasUpdate.PostLayout)
+        protected override void OnEnable()
         {
-            UpdateSelection();
+            base.OnEnable();
+            CanvasUpdateRegistry.RegisterCanvasElementForLayoutRebuild(this);
         }
-    }
 
-    public virtual void LayoutComplete() { }
-
-    public virtual void GraphicUpdateComplete() { }
-
-    public void SetLayoutHorizontal()
-    {
-        _drivenRectTracker.Clear();
-        var rect = viewport.rect;
-        var w = rect.width;
-        var h = rect.height;
-        _drivenRectTracker.Add(
-          this,
-          content,
-          DrivenTransformProperties.Anchors |
-          DrivenTransformProperties.AnchoredPositionY |
-          DrivenTransformProperties.SizeDelta);
-        // Set the content size to match the viewport.
-        content.anchorMin = Vector2.zero;
-        content.anchorMax = Vector2.up;
-        content.sizeDelta = new Vector2(content.childCount * w, 0);
-        content.anchoredPosition = new Vector2(content.anchoredPosition.x, 0);
-        for (var i = 0; i < content.childCount; i++)
+        protected override void OnDisable()
         {
-            var childRect = content.GetChild(i).GetComponent<RectTransform>();
+            base.OnDisable();
+            CanvasUpdateRegistry.UnRegisterCanvasElementForRebuild(this);
+        }
+
+        public virtual void Rebuild(CanvasUpdate executing)
+        {
+            if (executing == CanvasUpdate.PostLayout)
+            {
+                UpdateSelection();
+            }
+        }
+
+        public virtual void LayoutComplete() { }
+
+        public virtual void GraphicUpdateComplete() { }
+
+        public void SetLayoutHorizontal()
+        {
+            _drivenRectTracker.Clear();
+            var rect = viewport.rect;
+            var w = rect.width;
+            var h = rect.height;
             _drivenRectTracker.Add(
               this,
-              childRect,
+              content,
               DrivenTransformProperties.Anchors |
-              DrivenTransformProperties.AnchoredPosition |
+              DrivenTransformProperties.AnchoredPositionY |
               DrivenTransformProperties.SizeDelta);
-            childRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, i * w, w);
-            childRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, h);
-            childRect.localScale = Vector3.one;
+            // Set the content size to match the viewport.
+            content.anchorMin = Vector2.zero;
+            content.anchorMax = Vector2.up;
+            content.sizeDelta = new Vector2(content.childCount * w, 0);
+            content.anchoredPosition = new Vector2(content.anchoredPosition.x, 0);
+            for (var i = 0; i < content.childCount; i++)
+            {
+                var childRect = content.GetChild(i).GetComponent<RectTransform>();
+                _drivenRectTracker.Add(
+                  this,
+                  childRect,
+                  DrivenTransformProperties.Anchors |
+                  DrivenTransformProperties.AnchoredPosition |
+                  DrivenTransformProperties.SizeDelta);
+                childRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, i * w, w);
+                childRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, h);
+                childRect.localScale = Vector3.one;
+            }
+            LayoutRebuilder.ForceRebuildLayoutImmediate(content);
         }
-        LayoutRebuilder.ForceRebuildLayoutImmediate(content);
-    }
 
-    public void SetLayoutVertical() { }
+        public void SetLayoutVertical() { }
 
-    public virtual void OnBeginDrag(PointerEventData eventData)
-    {
-        IsDragging = true;
-    }
-
-    public virtual void OnDrag(PointerEventData eventData)
-    {
-        if (swipeEnabled)
+        public virtual void OnBeginDrag(PointerEventData eventData)
         {
-            ScrollPosition -= eventData.delta.x;
+            IsDragging = true;
         }
-        UpdateSelection();
-    }
 
-    public virtual void OnEndDrag(PointerEventData eventData)
-    {
-        if (Count > 0)
+        public virtual void OnDrag(PointerEventData eventData)
         {
-            var w = viewport.rect.size.x;
-            var predictedPos = ScrollPosition + _velocity * snapTime;
-            _snapPosition = Mathf.Round(predictedPos / w) * w;
-        }
-        IsDragging = false;
-    }
-
-    protected virtual void LateUpdate()
-    {
-        var notYetUpdated = true;
-        float newPosition;
-
-        if (Mathf.Abs(_snapPosition - ScrollPosition) < 0.001f)
-        {
-            _velocity = 0f;
-            newPosition = _snapPosition;
-        }
-        else
-        {
-            newPosition = Mathf.SmoothDamp(ScrollPosition, _snapPosition, ref _velocity, snapTime);
-        }
-        if (_prevPosition != newPosition)
-        {
-            _prevPosition = ScrollPosition = newPosition;
+            if (swipeEnabled)
+            {
+                ScrollPosition -= eventData.delta.x;
+            }
             UpdateSelection();
-            notYetUpdated = false;
         }
 
-        if (notYetUpdated && (_viewportWidth != viewport.rect.size.x || _contentWidth != content.rect.size.x))
+        public virtual void OnEndDrag(PointerEventData eventData)
         {
-            UpdateSelection();
-            _viewportWidth = viewport.rect.size.x;
-            _contentWidth = content.rect.size.x;
+            if (Count > 0)
+            {
+                var w = viewport.rect.size.x;
+                var predictedPos = ScrollPosition + _velocity * snapTime;
+                _snapPosition = Mathf.Round(predictedPos / w) * w;
+            }
+            IsDragging = false;
         }
-    }
 
-    private void UpdateSelection()
-    {
-        // Do something
-    }
+        protected virtual void LateUpdate()
+        {
+            var notYetUpdated = true;
+            float newPosition;
 
-    public void SkipBtnClicked()
-    {
-        prefab.SetActive(false); // temp
-    }
+            if (Mathf.Abs(_snapPosition - ScrollPosition) < 0.001f)
+            {
+                _velocity = 0f;
+                newPosition = _snapPosition;
+            }
+            else
+            {
+                newPosition = Mathf.SmoothDamp(ScrollPosition, _snapPosition, ref _velocity, snapTime);
+            }
+            if (_prevPosition != newPosition)
+            {
+                _prevPosition = ScrollPosition = newPosition;
+                UpdateSelection();
+                notYetUpdated = false;
+            }
 
-    public void NextBtnClicked()
-    {
-        _selectedTabIndex += 1;
-        _snapPosition = viewport.rect.width * _selectedTabIndex;
-    }
+            if (notYetUpdated && (_viewportWidth != viewport.rect.size.x || _contentWidth != content.rect.size.x))
+            {
+                UpdateSelection();
+                _viewportWidth = viewport.rect.size.x;
+                _contentWidth = content.rect.size.x;
+            }
+        }
 
-    public void BackBtnClicked()
-    {
-        _selectedTabIndex -= 1;
-        _snapPosition = viewport.rect.width * _selectedTabIndex;
-    }
+        private void UpdateSelection()
+        {
+            // Do something
+        }
 
-    public void StartEditingTutorialClicked()
-    {
+        public void SkipBtnClicked()
+        {
+            prefab.SetActive(false); // temp
+        }
 
-    }
+        public void NextBtnClicked()
+        {
+            _selectedTabIndex += 1;
+            _snapPosition = viewport.rect.width * _selectedTabIndex;
+        }
 
-    public void StartViewingTutorialClicked()
-    {
+        public void BackBtnClicked()
+        {
+            _selectedTabIndex -= 1;
+            _snapPosition = viewport.rect.width * _selectedTabIndex;
+        }
 
+        public void StartEditingTutorialClicked()
+        {
+            prefab.SetActive(false);
+            TutorialManager.Instance.StartNewMobileEditingTutorial();
+        }
+
+        public void StartViewingTutorialClicked()
+        {
+            Debug.Log("nay");
+            prefab.SetActive(false);
+        }
     }
 }
-
-
-
-
-
