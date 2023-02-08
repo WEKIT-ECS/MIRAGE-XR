72cc76bf73c086b9c0d955d5051bc9b2a2926053
Alex Shubin <icawke@gmail.com>
2023-02-08 16:45:30 +0300

    debug lines have been removed

--- Assets/MirageXR/Player/Prefabs/MRTK/AnimatedCursorWithCheck.cs
+++ Assets/MirageXR/Player/Prefabs/MRTK/AnimatedCursorWithCheck.cs
@@ -1,17 +1,9 @@
 using Microsoft.MixedReality.Toolkit.Input;
-using UnityEngine;
 
 public class AnimatedCursorWithCheck : AnimatedCursor
 {
-    protected override void Start()
-    {
-        Debug.LogWarning("-----AnimatedCursorWithCheck.Start");
-        base.Start();
-    }
-
     public override void OnPointerUp(MixedRealityPointerEventData eventData)
     {
-        Debug.Log("-----AnimatedCursorWithCheck.OnPointerUp");
         if (eventData?.InputSource?.Pointers != null)
         {
             base.OnPointerUp(eventData);
