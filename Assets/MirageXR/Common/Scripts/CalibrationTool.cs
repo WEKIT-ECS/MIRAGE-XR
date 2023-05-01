265e61e53ef7d1b210fdce94495dac75bceb1dfe
Alexander Shubin <icawke@gmail.com>
2023-05-01 05:01:35 +0300

    arfoundation calibration fix (#1365)
    * arfoundation calibration has been fixed
    
    * new behavior has been added

--- Assets/MirageXR/Common/Scripts/CalibrationTool.cs
+++ Assets/MirageXR/Common/Scripts/CalibrationTool.cs
@@ -34,7 +34,9 @@ namespace MirageXR
                 return;
             }
 
+            imageTarget.onTargetFound.RemoveAllListeners();
             imageTarget.onTargetFound.AddListener(OnTargetFound);
+            imageTarget.onTargetLost.RemoveAllListeners();
             imageTarget.onTargetLost.AddListener(OnTargetLost);
         }
 
