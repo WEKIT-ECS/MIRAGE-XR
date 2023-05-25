7ca870da6ad3a88d06bdfb3bb0d004f8728aeb9b
robhillman97 <robhillman97@gmail.com>
2023-05-25 07:35:40 +0100

    resolving errors in image marker code after vuforia update

--- Assets/MirageXR/Common/Scripts/ImageTarget/Manager/ImageTargetManagerVuforia.cs
+++ Assets/MirageXR/Common/Scripts/ImageTarget/Manager/ImageTargetManagerVuforia.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -9,14 +8,14 @@ public class ImageTargetManagerVuforia : ImageTargetManagerBase
 {
     public override async Task<bool> InitializationAsync()
     {
-        var vuforiaRuntime = VuforiaRuntime.Instance;
+        var vuforiaRuntime = VuforiaApplication.Instance;
 
-        if (vuforiaRuntime.InitializationState == VuforiaRuntime.InitState.NOT_INITIALIZED)
+        if (!vuforiaRuntime.IsInitialized)
         {
-            vuforiaRuntime.InitVuforia();
+            vuforiaRuntime.Initialize();
         }
 
-        while (vuforiaRuntime.InitializationState != VuforiaRuntime.InitState.INITIALIZED)
+        while (!vuforiaRuntime.IsInitialized)
         {
             await Task.Yield();
         }
@@ -32,12 +31,6 @@ public class ImageTargetManagerVuforia : ImageTargetManagerBase
 
             mainCamera.gameObject.AddComponent<VuforiaBehaviour>();
             mainCamera.gameObject.AddComponent<DefaultInitializationErrorHandler>();
-        }
-
-        var manager = VuforiaManager.Instance;
-        while (!manager.Initialized)
-        {
-            await Task.Yield();
         }
 
         _isInitialized = true;
@@ -65,12 +58,7 @@ public class ImageTargetManagerVuforia : ImageTargetManagerBase
             throw new NullReferenceException($"Can't find {imageTarget.imageTargetName}");
         }
 
-        var trackable = imageTargetVuforia.imageTargetBehaviour.Trackable;
-        var objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
-        var dataset = objectTracker.GetDataSets().FirstOrDefault(t => t.GetTrackables().Contains(trackable));
-        objectTracker.DeactivateDataSet(dataset);
-        objectTracker.DestroyDataSet(dataset, true);
-        _images.Remove(imageTarget.imageTargetName);
+        Destroy(imageTarget);
     }
 
     protected override void OnEnable()
@@ -92,21 +80,10 @@ public class ImageTargetManagerVuforia : ImageTargetManagerBase
 
     private static ImageTargetBehaviour CreateImageTargetFromTexture(Texture2D texture, float widthInMeters, string targetName)
     {
-        var objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
-        var runtimeImageSource = objectTracker.RuntimeImageSource;
-        runtimeImageSource.SetImage(texture, widthInMeters, targetName);
 
-        var dataset = objectTracker.CreateDataSet();
+        var trackableBehaviour = VuforiaBehaviour.Instance.ObserverFactory.CreateImageTarget(texture, widthInMeters, targetName);
 
-        if (dataset == null)
-        {
-            throw new Exception("Can't create dataset");
-        }
-
-        var trackableBehaviour = dataset.CreateTrackable(runtimeImageSource, targetName);
-
-        objectTracker.ActivateDataSet(dataset);
-
-        return trackableBehaviour as ImageTargetBehaviour;
+        return trackableBehaviour;
+        return null;
     }
 }
