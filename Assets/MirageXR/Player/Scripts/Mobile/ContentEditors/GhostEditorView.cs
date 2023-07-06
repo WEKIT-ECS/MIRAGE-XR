ddfbbce8e5e465c078791f88e102cf381ad744f9
Alex Shubin <icawke@gmail.com>
2023-07-06 13:26:17 +0300

    local rotation calculation has been fixed

--- Assets/MirageXR/Player/Scripts/Mobile/ContentEditors/GhostEditorView.cs
+++ Assets/MirageXR/Player/Scripts/Mobile/ContentEditors/GhostEditorView.cs
@@ -9,6 +9,7 @@ public class GhostEditorView : PopupEditorBase
 {
     private const string MALE_TYPE = "GhosttrackPrefab";
     private const string FEMALE_TYPE = "GhosttrackPrefabFemale";
+
     public override ContentType editorForType => ContentType.GHOST;
 
     [SerializeField] private Toggle _toggleMale;
