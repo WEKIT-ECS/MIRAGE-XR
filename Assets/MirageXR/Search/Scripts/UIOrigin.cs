050f4104504cce5df86a461c5e473b3c990d3716
Abbas Jafari <a85jafari@gmail.com>
2023-07-26 13:44:37 +0200

    Missing files from develop is pulled

--- Assets/MirageXR/Search/Scripts/UIOrigin.cs
+++ Assets/MirageXR/Search/Scripts/UIOrigin.cs
@@ -21,17 +21,17 @@ public class UIOrigin : MonoBehaviour
 
     private List<float> _raycastResults = new List<float>();
 
-        /// <summary>
-        /// Singleton instance of the placement script
-        /// </summary>
-        public static UIOrigin Instance { get; private set; }
+    /// <summary>
+    /// Singleton instance of the placement script
+    /// </summary>
+    public static UIOrigin Instance { get; private set; }
 
     public float CurrentFloorYPosition() { return _currentPosition.y; }
 
-        // Sets the main camera as the target that the aura should follow
-        private void Awake()
-        {
-            Instance = this;
+    // Sets the main camera as the target that the aura should follow
+    private void Awake()
+    {
+        Instance = this;
 
         // Attach mixed reality camera as the follow target.
         _followTarget = GameObject.FindWithTag("MainCamera").transform;
@@ -64,29 +64,29 @@ public class UIOrigin : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(_followTarget.forward, Vector3.up).normalized, Vector3.up);
     }
 
-        // Calculates the floor height
-        // Casts a given number of rays in a circular shape around the user to find the floor heights
-        // It finds the lowest found point, spans a 20cm zone above this and then takes the highest raycast hit within this zone
-        // This should prevent the aura from jumping onto furniture, e.g. if the user sits on a chair
-        // It should also lower the probability that the aura is placed underneath the floor, e.g. in environments with steps
-        private float GetFloorHeight(Vector3 centerPos, float radius, int numberOfRaycasts)
-        {
-            float minimumValue = centerPos.y;
+    // Calculates the floor height
+    // Casts a given number of rays in a circular shape around the user to find the floor heights
+    // It finds the lowest found point, spans a 20cm zone above this and then takes the highest raycast hit within this zone
+    // This should prevent the aura from jumping onto furniture, e.g. if the user sits on a chair
+    // It should also lower the probability that the aura is placed underneath the floor, e.g. in environments with steps
+    private float GetFloorHeight(Vector3 centerPos, float radius, int numberOfRaycasts)
+    {
+        float minimumValue = centerPos.y;
 
         _raycastResults.Clear();
 
-            // create rays circular around a center position with a given radius
+        // create rays circular around a center position with a given radius
 
-            for (int i = 0; i < numberOfRaycasts; i++)
-            {
-                float radians = 2 * Mathf.PI / numberOfRaycasts * i;
+        for (int i = 0; i < numberOfRaycasts; i++)
+        {
+            float radians = 2 * Mathf.PI / numberOfRaycasts * i;
 
-                float vertical = Mathf.Sin(radians);
-                float horizontal = Mathf.Cos(radians);
+            float vertical = Mathf.Sin(radians);
+            float horizontal = Mathf.Cos(radians);
 
-                Vector3 spawnDir = new Vector3(horizontal, centerPos.y, vertical);
+            Vector3 spawnDir = new Vector3(horizontal, centerPos.y, vertical);
 
-                Vector3 raycastOrigin = centerPos + spawnDir * radius;
+            Vector3 raycastOrigin = centerPos + spawnDir * radius;
 
             Ray ray = new Ray(raycastOrigin, Vector3.down);
             Debug.DrawLine(raycastOrigin, raycastOrigin + 2f * ray.direction);
@@ -97,7 +97,7 @@ public class UIOrigin : MonoBehaviour
             }
         }
 
-            float floorHeight;
+        float floorHeight;
 
         // if we did not find the floor with any raycast, we just take a default height were we assume the floor to be
         if (_raycastResults.Count == 0)
@@ -118,8 +118,6 @@ public class UIOrigin : MonoBehaviour
             }
         }
 
-            return floorHeight;
-        }
+        return floorHeight;
     }
-
 }
