db3c811453a88698114f9edafaa4a0a457aab324
Fridolin Wild <fridolin.wild@gmail.com>
2023-12-22 14:13:05 +0000

    adding tests to check if credentials are ok

--- Assets/MirageXR/Managers/ExceptionManager/ExceptionManager.cs
+++ Assets/MirageXR/Managers/ExceptionManager/ExceptionManager.cs
@@ -11,52 +11,46 @@ namespace MirageXR
    /// to ask to send a report to sentry.io; the main exceptions registered for are:
    /// Null Reference, Divide by Zero, Out of Memory, Index Out of Range.
    /// </summary>
-
    public class ExceptionManager : MonoBehaviour
    {
+        // the Sentry instance
+        public SentrySdk sentry;
 
-       // the Sentry instance
-       public SentrySdk sentry; 
+        // Sentry key
+        private string _dsn = "https://b23911205078e7a81bf1489e8aa0fabe@o4506320008118272.ingest.sentry.io/4506320009428992";
 
-       // Sentry key
-       private const string SentryDsn = "https://b23911205078e7a81bf1489e8aa0fabe@o4506320008118272.ingest.sentry.io/4506320009428992";
-
-       private void Awake()
-       {
-           AppLog.Log("Installing hook for exceptions", LogLevel.INFO );
-           sentry ??= new GameObject("ExceptionManagerSentry").AddComponent<SentrySdk>();
-           sentry.Dsn = SentryDsn;
-
-           Application.logMessageReceived += LogCaughtException;
-           DontDestroyOnLoad(gameObject); 
-       }
-
-       public void LogCaughtException(string logText, string stackTrace, LogType logType)
-       {
-
+        public void LogCaughtException(string logText, string stackTrace, LogType logType)
+        {
            if (logType == LogType.Exception)
            {
-
-        AppLog.Log($"ExceptionManager: [{logType}] {logText}, trace: {stackTrace}", LogLevel.CRITICAL);
-        SentrySdk.CaptureMessage($"ExceptionManager: [{logType}] {logText}, trace: {stackTrace}");
+                AppLog.Log($"ExceptionManager: received Exception: [{logType}] {logText}, trace: {stackTrace}", LogLevel.INFO);
+                SentrySdk.CaptureMessage($"ExceptionManager: [{logType}] {logText}, trace: {stackTrace}");
 /*
-              RootView_v2.Instance.dialog.ShowMiddle(
-                 "A serious error happened!",
-                 "This may cause the app to become unstable and we recommend restarting the app, especially if not editing. Send error report to the development team?",
-                 "OK", () => SentrySdk.CaptureMessage("ExceptionManager: ["+logType+"]" + logText + ", trace: " + stackTrace),
-                 "Cancel", () => AppLog.Log("-> User chose not to report this serious exception", LogLevel.INFO),
-                 true);
+                RootView_v2.Instance.dialog.ShowMiddle(
+                   "A serious error happened!",
+                   "This may cause the app to become unstable and we recommend restarting the app, especially if not editing. Send error report to the development team?",
+                   "OK", () => SentrySdk.CaptureMessage("ExceptionManager: ["+logType+"]" + logText + ", trace: " + stackTrace),
+                   "Cancel", () => AppLog.Log("-> User chose not to report this serious exception", LogLevel.INFO),
+                   true);
 */
-
            }
+        }
 
-      }
+        private void Awake()
+        {
+            AppLog.Log("Installing hook for exceptions", LogLevel.INFO);
 
-      private void OnDestroy()
-      {
-         Application.logMessageReceived -= LogCaughtException;
-      }
+            sentry ??= new GameObject("ExceptionManagerSentry").AddComponent<SentrySdk>();
+            sentry.Dsn = _dsn;
 
+            Application.logMessageReceived += LogCaughtException;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+           Application.logMessageReceived -= LogCaughtException;
+        }
    }
-
 }
