17c4162062f7eb0a784ce401602dec9089c28d40
Alex Shubin <icawke@gmail.com>
2023-12-29 18:08:02 +0300

    tests has been removed

--- Assets/MirageXR/Managers/ExceptionManager/ExceptionManager.cs
+++ Assets/MirageXR/Managers/ExceptionManager/ExceptionManager.cs
@@ -11,10 +11,8 @@ namespace MirageXR
 
    public class ExceptionManager : MonoBehaviour
    {
-       // the Sentry instance
        [SerializeField] private SentrySdk sentry; 
 
-       // Sentry key
        private const string SENTRY_DSN = "https://b23911205078e7a81bf1489e8aa0fabe@o4506320008118272.ingest.sentry.io/4506320009428992";
 
        public void Initialize()
@@ -31,14 +29,6 @@ namespace MirageXR
            if (logType == LogType.Exception)
            {
                 SentrySdk.CaptureMessage($"ExceptionManager: [{logType}] {logText}, trace: {stackTrace}");
-                /*
-                RootView_v2.Instance.dialog.ShowMiddle(
-                   "A serious error happened!",
-                   "This may cause the app to become unstable and we recommend restarting the app, especially if not editing. Send error report to the development team?",
-                   "OK", () => SentrySdk.CaptureMessage("ExceptionManager: ["+logType+"]" + logText + ", trace: " + stackTrace),
-                   "Cancel", () => AppLog.Log("-> User chose not to report this serious exception", LogLevel.INFO),
-                   true);
-                */
            }
        }
 
