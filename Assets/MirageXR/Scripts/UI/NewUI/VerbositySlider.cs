e0dc399a3776dcf1895baa66684449813ff14946
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2024-03-06 17:28:05 +0100

    connected log level slider and added text effect to visualize activated levels

--- Assets/MirageXR/Scripts/UI/NewUI/VerbositySlider.cs
+++ Assets/MirageXR/Scripts/UI/NewUI/VerbositySlider.cs
@@ -1,3 +1,4 @@
+using i5.Toolkit.Core.VerboseLogging;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,37 +12,53 @@ namespace MirageXR
 		[SerializeField] private Slider _slider;
 		[SerializeField] private Transform[] _labelContainers;
 
-		private TextMeshPro[] _labels;
+		private TextMeshProUGUI[] _labels;
 		private Image[] _labelIcons;
+
+		private readonly Color inactiveColor = new Color(0.6862745f, 0.6862745f, 0.6862745f); // #AFAFAF
+		private readonly Color activeColor = Color.white;
 
 		private int _logLevel;
 
 		private void Awake()
 		{
-			_labels = new TextMeshPro[_labelContainers.Length];
+			_labels = new TextMeshProUGUI[_labelContainers.Length];
 			_labelIcons = new Image[_labelContainers.Length];
 
-			for (int i=0;i< _labelContainers.Length;i++)
+			for (int i = 0; i < _labelContainers.Length; i++)
 			{
-				_labels[i] = _labelContainers[i].GetComponentInChildren<TextMeshPro>();
+				_labels[i] = _labelContainers[i].GetComponentInChildren<TextMeshProUGUI>();
 				_labelIcons[i] = _labelContainers[i].GetComponentInChildren<Image>();
 			}
+		}
+
+		private void OnEnable()
+		{
+			_logLevel = (int)AppLog.MinimumLogLevel;
+			AdjustUI();
 		}
 
 		public void OnSliderValueChanged()
 		{
 			_logLevel = (int)_slider.value;
+			AppLog.MinimumLogLevel = (LogLevel)_logLevel;
+			AppLog.LogInfo("Changed log level to " + _logLevel, this);
+			AdjustUI();
 		}
 
 		private void AdjustUI()
 		{
-			for (int i=0;i<_labelContainers.Length;i++)
+			for (int i = 0; i < _labels.Length; i++)
 			{
-				if (i < _logLevel)
+				if (i <= _logLevel)
 				{
+					_labels[i].color = activeColor;
+					_labelIcons[i].color = activeColor;
 				}
 				else
 				{
+					_labels[i].color = inactiveColor;
+					_labelIcons[i].color = inactiveColor;
 				}
 			}
 		}
