f8cee59b4ea400b77e271c70e584c120cf041d04
Fridolin Wild <fridolin.wild@gmail.com>
2024-03-27 17:43:08 +0000

    fixed broken tests; added settings for iOS

--- Assets/UnitTests/PlayModeTests/CalibrationTests.cs
+++ Assets/UnitTests/PlayModeTests/CalibrationTests.cs
@@ -202,8 +202,15 @@ namespace Tests
             LoadTestArlemModels();
 
             // audio listener cannot be added with helper function
-            var audioListener = new GameObject("Audio Listener");
-            audioListener.AddComponent<AudioListener>();
+            // FW (2024-03-27) added a check to avoid having several audio listeners in the scene
+            if (!GameObject.Find("Audio Listener"))
+            {
+                var audioListener = new GameObject("Audio Listener"); 
+                audioListener.AddComponent<AudioListener>();
+            } else
+            {
+                var audioListener = GameObject.Find("Audio Listener");
+            }
 
             // create and configure content managers or UI elements needed to start an activity
             GenerateGameObjectWithComponent<Maggie>("Maggie");
