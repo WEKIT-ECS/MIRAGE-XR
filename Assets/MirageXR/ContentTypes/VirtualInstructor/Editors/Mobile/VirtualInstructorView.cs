6fd5879a2849f967107b0d908d5b69fc60a8f99b
Sergei Iliasov <sergeyilyasov@gmail.com>
2024-04-19 01:15:24 +0200

    added virtual instructor screen

---
+++ Assets/MirageXR/ContentTypes/VirtualInstructor/Editors/Mobile/VirtualInstructorView.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualInstructorView : PopupEditorBase
+{
+    public override ContentType editorForType => ContentType.MODEL; // TODO: add content type
+}
