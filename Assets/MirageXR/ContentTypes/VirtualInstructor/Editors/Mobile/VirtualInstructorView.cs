4e97838774e00e5a3ad8f8b0c37d3724306db972
Sergei Iliasov <sergeyilyasov@gmail.com>
2024-04-19 03:09:20 +0200

    added new content type

--- Assets/MirageXR/ContentTypes/VirtualInstructor/Editors/Mobile/VirtualInstructorView.cs
+++ Assets/MirageXR/ContentTypes/VirtualInstructor/Editors/Mobile/VirtualInstructorView.cs
@@ -1,8 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VirtualInstructorView : PopupEditorBase
 {
-    public override ContentType editorForType => ContentType.MODEL; // TODO: add content type
+    private const float HIDED_SIZE = 100f;
+    private const float HIDE_ANIMATION_TIME = 0.5f;
+    
+    [Space]
+    [SerializeField] private Button _btnArrow;
+    [SerializeField] private RectTransform _panel;
+    [SerializeField] private GameObject _arrowDown;
+    [SerializeField] private GameObject _arrowUp;
+    public override ContentType editorForType => ContentType.VIRTUALINSTRUCTOR; 
+    
+    public override void Initialization(Action<PopupBase> onClose, params object[] args)
+    {
+        _showBackground = false;
+        base.Initialization(onClose, args);
+        
+        _btnArrow.onClick.AddListener(OnArrowButtonPressed);
+
+        RootView_v2.Instance.HideBaseView();
+    }
+    private void OnArrowButtonPressed()
+    {
+        if (_arrowDown.activeSelf)
+        {
+            var hidedSize = HIDED_SIZE;
+            _panel.DOAnchorPosY(-_panel.rect.height + hidedSize, HIDE_ANIMATION_TIME);
+            _arrowDown.SetActive(false);
+            _arrowUp.SetActive(true);
+        }
+        else
+        {
+            _panel.DOAnchorPosY(0.0f, HIDE_ANIMATION_TIME);
+            _arrowDown.SetActive(true);
+            _arrowUp.SetActive(false);
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        RootView_v2.Instance.ShowBaseView();
+    }
+
 }
