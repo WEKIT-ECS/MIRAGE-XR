8356306ac3bb0c2968f470c599b2b4b66a50c027
AKissMail <andreas@kissmehl.de>
2024-04-24 21:17:46 +0200

    Clean up some death code and add documentation.

--- Assets/MirageXR/Scripts/UI/NewUI/ToggleTaskStation.cs
+++ Assets/MirageXR/Scripts/UI/NewUI/ToggleTaskStation.cs
@@ -1,40 +1,48 @@
 using UnityEngine;
-using UnityEngine.UI; // Zugriff auf die UI-Komponenten
-
+using UnityEngine.UI; 
 
 namespace MirageXR
 {
-
-// Steuert die Sichtbarkeit basierend auf einem Toggle
+    /// <summary>
+    /// Class for controlling the visibility of a toggle button in Unity.
+    /// </summary>
+    /// <remarks>
+    /// This class is used to control the visibility of a toggle button in Unity. It is designed to be used in conjunction with the Toggle component from the UnityEngine.UI namespace.
+    /// </remarks>
     public class ToggleVisibilityController : MonoBehaviour
     {
-        public Toggle myToggle; // Der Toggle, der die Sichtbarkeit steuert
-        private TaskStationVisibilityController visibilityController; // Referenz zum VisibilityController
+        public Toggle myToggle;
 
+        /// <summary>
+        /// Controls the visibility of a game object based on a toggle.
+        /// </summary>
+        private TaskStationVisibilityController visibilityController;
+
+        /// <summary>
+        /// This method is called when the ToggleVisibilityController starts.
+        /// </summary>
+        /// <remarks>
+        /// The Start method is responsible for initializing the ToggleVisibilityController by subscribing to the onValueChanged event of the Toggle component and assigning the visibilityController variable with an instance of the TaskStationVisibilityController.
+        /// </remarks>
         void Start()
         {
-            // Überprüfe, ob der Toggle zugewiesen wurde
             if (myToggle != null)
             {
-                // Füge den Listener hinzu, der aufgerufen wird, wenn der Toggle seinen Wert ändert
                 myToggle.onValueChanged.AddListener(ToggleValueChanged);
-                // Suche den VisibilityController im Scene
                 visibilityController = FindObjectOfType<TaskStationVisibilityController>();
             }
         }
 
+        /// <summary>
+        /// Method called when the value of the toggle is changed.
+        /// </summary>
+        /// <param name="isOn">The new value of the toggle.</param>
         private void ToggleValueChanged(bool isOn)
         {
-            UnityEngine.Debug.Log("Toggle............");
-            // Setze die Sichtbarkeit des Ziel-GameObjects
             if (visibilityController != null)
             {
                 UnityEngine.Debug.Log(visibilityController);
                 visibilityController.SetVisibility(isOn);
-            }
-            else
-            {
-                UnityEngine.Debug.Log("visibilityController = null ............");
             }
         }
     }
