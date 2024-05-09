237d946f9c30ae90818978e5e86d9d599e82a85f
AKissMail <andreas@kissmehl.de>
2024-05-09 12:15:10 +0200

    Update bes on the Feedback and add comments for Documentation.

--- Assets/MirageXR/Managers/AIManager/AIManager.cs
+++ Assets/MirageXR/Managers/AIManager/AIManager.cs
@@ -8,20 +8,45 @@ using UnityEngine;
 
 namespace MirageXR
 {
+    /// <summary>
+    /// The AIManager class is responsible for managing AI services such as speech recognition, natural language processing, and text-to-speech conversion.
+    /// </summary>
     public class AIManager
     {
+        /// <summary>
+        /// The URL of the AI API server.
+        /// </summary>
         private string _url;
+
+        /// <summary>
+        /// The username used for authentication in the AI manager.
+        /// </summary>
         private string _username;
         private string _password;
 
-        private Token _token;
+        /// <summary>
+        /// Represents a token used for authentication in the AIManager class.
+        /// </summary>
+        private string _token; 
 
-        //private List<OptionsResponse> _models;
+        /// <summary>
+        /// The list of Language Learning Models (LLM) available for the AI Manager.
+        /// </summary>
         private List<AIModel> _llmModels = new();
+
+        /// Represents the list of speech-to-text models available for the AIManager.
         private List<AIModel> _sttModels = new();
+
+        /// <summary>
+        /// Represents the list of text-to-speech (TTS) models available for conversion.
+        /// </summary>
         private List<AIModel> _ttsModels = new();
 
 
+        /// <summary>
+        /// Initializes the AIManager by reading configuration settings, authenticating the user, and setting available models.
+        /// </summary>
+        /// <returns>A Task representing the asynchronous operation.</returns>
         public async Task InitializeAsync()
         {
             try
@@ -36,33 +61,49 @@ namespace MirageXR
             }
         }
 
-        public List<AIModel> GetLLMModels()
+        /// <summary>
+        /// Retrieves the LLM (Language and Linguistic Model) models available in the AIManager.
+        /// </summary>
+        /// <returns>A list of AIModel objects representing the LLM models.</returns>
+        public List<AIModel> GetLlmModels()
         {
             return _llmModels;
         }
-        
-        public List<AIModel> GetSTTModels()
+
+        /// <summary>
+        /// Returns a list of Speech-To-Text (STT) models available for use.
+        /// </summary>
+        /// <returns>A list of AIModel objects representing the available STT models</returns>
+        public List<AIModel> GetSttModels()
         {
             return _sttModels;
         }
-        
-        public List<AIModel> GetTTSModels()
+
+        /// <summary>
+        /// Retrieves the list of Text-to-Speech (TTS) models.
+        /// </summary>
+        /// <returns>The list of TTS models available in the AI manager.</returns>
+        public List<AIModel> GetTtsModels()
         {
             return _ttsModels;
         }
 
+        /// <summary>
+        /// Sets the AI models based on the provided list of endpoint models.
+        /// </summary>
+        /// <param name="endpointModels">The list of endpoint models.</param>
         private void SetModels(List<AIModel> endpointModels)
         {
-            
             foreach (var model in endpointModels)
             {
                 AddModelBasedOnEndpointName(model);
-                UnityEngine.Debug.Log("Model "+model);
             }
-            UnityEngine.Debug.Log("Set models done! LLM:"+_llmModels.Count+" TTS:"+_ttsModels.Count+ "STT:"+_sttModels.Count );
-
         }
 
+        /// <summary>
+        /// Adds the given AIModel to the appropriate list based on the endpoint name.
+        /// </summary>
+        /// <param name="model">The AIModel to be added.</param>
         private void AddModelBasedOnEndpointName(AIModel model)
         {
             if (model.EndpointName == "listen/")
@@ -79,6 +120,10 @@ namespace MirageXR
             }
         }
 
+        /// <summary>
+        /// Reads the configuration file and initializes the AIManager.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation.</returns>
         private async Task ReadConfig()
         {
             const string fileName = "AI_Server";
@@ -135,23 +180,12 @@ namespace MirageXR
             _password = password;
         }
 
-        /** Do we need that one?
-         *  See GetLLMModels, GetSTTModels and GetTTSModels.
-         * public async Task<List<OptionsResponse>> GetModelsAsync()
-        
-        {
-            try
-            {
-                _models ??= await AiServices.GetAvailableModelsAsync(_url, _token);
-                return _models;
-            }
-            catch (Exception e)
-            {
-                AppLog.LogError(e.ToString());
-                return null;
-            }
-        }
-         */
+        /// <summary>
+        /// Converts speech to text using the specified audio clip and model.
+        /// </summary>
+        /// <param name="audioClip">The audio clip that should be transcribed.</param>
+        /// <param name="model">The model that should transcribe the audio.</param>
+        /// <returns>A string with the transcribed text or null if an error occurs.</returns>
         public async Task<string> ConvertSpeechToTextAsync(AudioClip audioClip, string model)
         {
             try
@@ -165,6 +199,13 @@ namespace MirageXR
             }
         }
 
+        /// <summary>
+        /// Sends a message to the AI assistant for processing.
+        /// </summary>
+        /// <param name="model">The target model for processing the message.</param>
+        /// <param name="message">The message provided by the user.</param>
+        /// <param name="context">The context provided by the instructor.</param>
+        /// <returns>A task representing the asynchronous operation. The result is a string with the result of the operation.</returns>
         public async Task<string> SendMessageToAssistantAsync(string model, string message, string context)
         {
             try
@@ -178,6 +219,12 @@ namespace MirageXR
             }
         }
 
+        /// <summary>
+        /// Converts a text message to speech using the specified model.
+        /// </summary>
+        /// <param name="message">The text message to be converted to speech.</param>
+        /// <param name="model">The model to be used for text-to-speech conversion.</param>
+        /// <returns>An async task that represents the asynchronous operation. The task result contains the audio clip representing the converted speech.</returns>
         public async Task<AudioClip> ConvertTextToSpeechAsync(string message, string model)
         {
             try
