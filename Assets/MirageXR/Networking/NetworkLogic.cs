6608b9c0a1e65b65fcfd8b4f353926d5ba508efe
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2024-06-13 15:41:12 +0200

    networking configuration demo

---
+++ Assets/MirageXR/Networking/NetworkLogic.cs
@@ -0,0 +1,20 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MirageXR
+{
+	public class NetworkLogic : NetworkBehaviour, IPlayerJoined, IPlayerLeft
+	{
+		public void PlayerJoined(PlayerRef player)
+		{
+			Debug.Log("Player joined: " + player);
+		}
+
+		public void PlayerLeft(PlayerRef player)
+		{
+			Debug.Log("Player left: " + player);
+		}
+	}
+}
