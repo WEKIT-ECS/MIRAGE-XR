62fee4b7839edaf60954f3f9259c01a6a6a14699
Alex Shubin <icawke@gmail.com>
2024-06-26 21:33:50 +0300

    openAi config fix

--- Assets/MirageXR/Player/Scripts/RootObject.cs
+++ Assets/MirageXR/Player/Scripts/RootObject.cs
@@ -131,7 +131,7 @@ namespace MirageXR
                 _cameraCalibrationChecker.Initialization();
                 _platformManager.Initialization();
                 await _openAIManager.InitializeAsync();
-                await _aiManager.InitializeAsync();
+                //await _aiManager.InitializeAsync();
                 _activityManager.Subscription();
 
                 _isInitialized = true;
