e65e5bf0a2ae76a24a484c55b648c77ba735628f
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2024-08-29 21:54:21 +0200

    added natural hand placement if not set by external tracking

--- Assets/MirageXR/ReadyPlayerMe/Scripts/HandController.cs
+++ Assets/MirageXR/ReadyPlayerMe/Scripts/HandController.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 namespace MirageXR
 {
-    public class HandController : MonoBehaviour
-    {
-        [SerializeField] private Transform _headTarget;
-        [SerializeField] private Transform _bodyTarget;
-        [SerializeField] private Transform _elbowHint;
+	public class HandController : MonoBehaviour
+	{
+		[Header("References")]
+		[SerializeField] private Transform _headTarget;
+		[SerializeField] private Transform _bodyTarget;
+		[SerializeField] private Transform _elbowHint;
 		[SerializeField] private bool _isLeftHand;
 
+		[Header("Configuration Values")]
+		[SerializeField] private Vector3 _handHipOffset = new Vector3(0.3f, 0, 0);
+		[SerializeField] private float _handInertia = 0.995f;
+		[SerializeField] private float _handDamping = 0.95f;
+		[SerializeField] private float _elbowWideness = 0.8f;
+
 		private Vector3 _currentElbowHintPosition;
+
+
+		private Vector3 _handTargetPosition;
+		private Vector3 _handAcceleration;
+
+		/// <summary>
+		/// If checked, the position of the hand target is left as is; otherwise, the script will find a natural position for the hands
+		/// </summary>
+		[field: Tooltip("If checked, the position of the hand target is left as is so that other scripts can position the hand; otherwise, the script will find a natural position for the hand target")]
+		[field: SerializeField]
+		public bool HandPositionSetExternally { get; set; } = true;
 
 		private void Update()
 		{
 			_currentElbowHintPosition = Vector3.Lerp(_headTarget.position, _bodyTarget.position, 0.8f);
 			_currentElbowHintPosition -= _bodyTarget.forward;
 			float sideFactor = _isLeftHand ? -1f : 1f;
-			_currentElbowHintPosition += sideFactor * _bodyTarget.right * 0.8f;
+			_currentElbowHintPosition += sideFactor * _bodyTarget.right * _elbowWideness;
 			_elbowHint.position = _currentElbowHintPosition;
+
+			if (!HandPositionSetExternally)
+			{
+				// set the hand to a plausible position relative to the body / hip
+				_handTargetPosition = _bodyTarget.position +
+					_bodyTarget.rotation *
+					Vector3.Scale(new Vector3(sideFactor, 0, 0), _handHipOffset);
+				_handAcceleration = _handDamping * (_handInertia * _handAcceleration + (1f - _handInertia) * (_handTargetPosition - transform.position));
+				Debug.Log(_handAcceleration);
+				transform.position += _handAcceleration;
+			}
 		}
 	}
 }
