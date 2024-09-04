a823e37f8fc7e0363cceab95c562ed24e2bf78b6
Sergei Iliasov <sergeyilyasov@gmail.com>
2024-09-05 00:44:27 +0200

    added view and controller for the main screen

---
+++ Assets/MirageXR/Player/Scripts/Spatial/MainScreenSpatialView.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Utility.UiKit.Runtime.MVC;
+using Utility.UiKit.Runtime.Extensions;
+
+namespace MirageXR
+{
+    public class MainScreenSpatialView : View
+    {
+        [Header("Buttons")]
+        [SerializeField] private Button _buttonSidebarCollapse;
+        [SerializeField] private Button _buttonSidebarExpand;
+        [SerializeField] private Button _button1;
+        [SerializeField] private Button _buttonSorting;
+        [SerializeField] private Button _buttonAddNewActivity;
+        [Header("GameObjects")]
+        [SerializeField] private GameObject _sidebarOpened;
+        [SerializeField] private GameObject _sidebarClosed;
+        
+        public void SetActionOnButtonSidebarCollapseClick(UnityAction action) => _buttonSidebarCollapse.SafeSetListener(action);
+        public void SetActionOnButtonSidebarExpandClick(UnityAction action) => _buttonSidebarExpand.SafeSetListener(action);
+        public void SetActionOnButtonSortingClick(UnityAction action) => _buttonSorting.SafeSetListener(action);
+        public void SetActionOnButtonAddNewActivityClick(UnityAction action) => _buttonAddNewActivity.SafeSetListener(action);
+    }
+}
