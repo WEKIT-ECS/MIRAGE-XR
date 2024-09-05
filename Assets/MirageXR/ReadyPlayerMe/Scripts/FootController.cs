8d27096da15607c3d9b0c896662816cd9f842cd3
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2024-09-05 13:34:45 +0200

    prepared toe targets for avatar

--- Assets/MirageXR/ReadyPlayerMe/Scripts/FootController.cs
+++ Assets/MirageXR/ReadyPlayerMe/Scripts/FootController.cs
@@ -1,6 +1,3 @@
-using Fusion;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MirageXR
@@ -11,6 +8,7 @@ namespace MirageXR
 		[SerializeField] private Transform _headTarget;
 		[SerializeField] private Transform _kneeHint;
 		[SerializeField] private Transform _bodyTarget;
+		[SerializeField] private Transform _toeTarget;
 		[SerializeField] private FootController _otherFoot;
 		[Header("Foot Placement")]
 		[SerializeField] private bool _isLeftFoot;
@@ -18,6 +16,7 @@ namespace MirageXR
 		[SerializeField] private float _footSpacing = 0.18f;
 		[Header("Stepping")]
 		//[SerializeField] private float _stepThreshold = 0.2f;
+		[SerializeField] private float _footAngleThreshold = 0f;
 		[SerializeField] private float _stepDistance = 0.25f;
 		[SerializeField] private float _stepHeight = 0.2f;
 		[SerializeField] private float _stepSpeed = 3.5f;
@@ -31,7 +30,7 @@ namespace MirageXR
 		private Pose _currentFootTargetPose;
 		private Vector3 _currentKneeHintPosition;
 		// step control points: the beginning and ending of a step
-		private Pose _previousFootTargetPose, _newFootTargetPose;
+		private Pose _previousFootGroundTargetPose, _footGroundTargetPose;
 		// keeps track of the current step progress; 0: step not started; 1: step completed
 		private float _stepLerpProgress = 0f;
 
@@ -57,8 +56,8 @@ namespace MirageXR
 		{
 			_footRotationOffset = transform.rotation;
 			_currentFootTargetPose = new Pose(transform.position, transform.rotation);
-			_newFootTargetPose = new Pose(transform.position, transform.rotation);
-			_previousFootTargetPose = new Pose(transform.position, transform.rotation);
+			_footGroundTargetPose = new Pose(transform.position, transform.rotation);
+			_previousFootGroundTargetPose = new Pose(transform.position, transform.rotation);
 			_stepLerpProgress = 1f;
 		}
 
@@ -75,36 +74,39 @@ namespace MirageXR
 				Destroy(_debugWidget);
 			}
 
-			PlaceFootTarget();
-			PlaceKneeHint();
+			ControlFoot();
+			ControlKnee();
 		}
 
-		private void PlaceFootTarget()
+		private void ControlFoot()
 		{
 			transform.position = _currentFootTargetPose.position + _footHeightOffset * Vector3.up;
 			transform.rotation = _currentFootTargetPose.rotation;
 
 			// look for the candidate where we would currently place the foot
-			Vector3 projectedVirtualFootPosition = _headTarget.position + (SidewaysVector * _footSpacing);
-			projectedVirtualFootPosition.y = _floorManager.GetFloorHeight(projectedVirtualFootPosition);
+			Vector3 footPositionCandidate = _bodyTarget.position + (SidewaysVector * _footSpacing);
+			footPositionCandidate.y = _floorManager.GetFloorHeight(footPositionCandidate);
 
-			float distance = Vector3.Distance(projectedVirtualFootPosition, _newFootTargetPose.position);
+			float distance = Vector3.Distance(footPositionCandidate, _footGroundTargetPose.position);
 			float dotProduct = Vector2.Dot(
-				new Vector2(_headTarget.forward.x, _headTarget.forward.z),
-				new Vector2(_newFootTargetPose.forward.x, _newFootTargetPose.forward.z));
+				new Vector2(_bodyTarget.forward.x, _bodyTarget.forward.z),
+				new Vector2(_footGroundTargetPose.forward.x, _footGroundTargetPose.forward.z));
 
 			// check if we need to make a step (and if we can take a step)
+			// we step if the distance of the foot candidate to our current foot placement is too high
+			// or if we rotated the body too far
 			if (!CurrentlyStepping && !_otherFoot.CurrentlyStepping
-				&& (distance > 1.05f * _stepDistance || dotProduct < 0))
+				&& (distance > 1.05f * _stepDistance || dotProduct < _footAngleThreshold))
 			{
+				// initiate the step
 				_stepLerpProgress = 0f;
 
-				Vector3 stepDirection = projectedVirtualFootPosition - _previousFootTargetPose.position;
+				Vector3 stepDirection = footPositionCandidate - _previousFootGroundTargetPose.position;
 				stepDirection.y = 0f;
 				stepDirection.Normalize();
 
-				_newFootTargetPose.position = projectedVirtualFootPosition + stepDirection * _stepDistance + _footHeightOffset * Vector3.up;
-				_newFootTargetPose.rotation = Quaternion.Euler(
+				_footGroundTargetPose.position = footPositionCandidate + stepDirection * _stepDistance + _footHeightOffset * Vector3.up;
+				_footGroundTargetPose.rotation = Quaternion.Euler(
 					_currentFootTargetPose.rotation.eulerAngles.x,
 					_headTarget.eulerAngles.y,
 					_currentFootTargetPose.rotation.eulerAngles.z);
@@ -112,26 +114,26 @@ namespace MirageXR
 			// progress the step further if we are currently making a step
 			if (_stepLerpProgress < 1f)
 			{
-				_currentFootTargetPose.position = Vector3.Lerp(_previousFootTargetPose.position, _newFootTargetPose.position, _stepLerpProgress);
+				_currentFootTargetPose.position = Vector3.Lerp(_previousFootGroundTargetPose.position, _footGroundTargetPose.position, _stepLerpProgress);
 				_currentFootTargetPose.position.y += Mathf.Sin(_stepLerpProgress * Mathf.PI) * _stepHeight;
 
-				_currentFootTargetPose.rotation = Quaternion.Slerp(_previousFootTargetPose.rotation, _newFootTargetPose.rotation, _stepLerpProgress);
+				_currentFootTargetPose.rotation = Quaternion.Slerp(_previousFootGroundTargetPose.rotation, _footGroundTargetPose.rotation, _stepLerpProgress);
 
 				_stepLerpProgress += Time.deltaTime * _stepSpeed;
 			}
 			else
 			{
-				_previousFootTargetPose.position = _newFootTargetPose.position;
-				_previousFootTargetPose.rotation = _newFootTargetPose.rotation;
+				_previousFootGroundTargetPose.position = _footGroundTargetPose.position;
+				_previousFootGroundTargetPose.rotation = _footGroundTargetPose.rotation;
 			}
 
 			if (_debugWidget != null)
 			{
-				_debugWidget.transform.position = _newFootTargetPose.position;
+				_debugWidget.transform.position = _footGroundTargetPose.position;
 			}
 		}
 
-		private void PlaceKneeHint()
+		private void ControlKnee()
 		{
 			_currentKneeHintPosition = Vector3.Lerp(_bodyTarget.position, _currentFootTargetPose.position, 0.5f);
 			// move the knee to the front to ensure correct bending
