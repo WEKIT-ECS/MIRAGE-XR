1b50da85b4fe0ecb1a1c1780adf5793dc07af9b7
Sergey Ilyasov <sergeyilyasov@gmail.com>
2024-09-12 01:27:20 +0200

    updated profile ui

--- Assets/MirageXR/Player/Scripts/Spatial/MainScreenSpatialViewController.cs
+++ Assets/MirageXR/Player/Scripts/Spatial/MainScreenSpatialViewController.cs
@@ -1,21 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MirageXR
 {
     public class MainScreenSpatialViewController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
