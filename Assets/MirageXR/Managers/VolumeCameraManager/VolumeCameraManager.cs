8f75d090ce132d4f5fc3d1a6a980ddd69ee9cf4b
Fridolin Wild <fridolin.wild@gmail.com>
2024-10-15 14:59:07 +0100

    refactored VolumeCameraManager to load MRTk only on non-VisionOS platforms

--- Assets/MirageXR/Managers/VolumeCameraManager/VolumeCameraManager.cs
+++ Assets/MirageXR/Managers/VolumeCameraManager/VolumeCameraManager.cs
@@ -8,44 +8,55 @@ using UnityEngine;
 
 namespace MirageXR
 {
+    /// <summary>
+    /// Responsible for initialising VisionOS components if on VisionOS, or MRTk if otherwise
+    /// </summary>
     public class VolumeCameraManager : MonoBehaviour
     {
         [SerializeField]
-        private GameObject mixedRealityPlayspace;
-        [SerializeField]
-        private GameObject volumeCamera;
-        [SerializeField]
-        private MixedRealityToolkit mixedRealityToolkit;
-        [SerializeField]
-        private MRTKHardwareRig mRTKHardwareRig;
-        [SerializeField]
-        private MixedRealityInputModule mixedRealityInputModule;
-        [SerializeField]
-        private GazeProvider gazeProvider;
+        [Tooltip("Drag & drop the MixedRealityPlayspace gameobject here, so the MRTk Hardware Rig component can be added.")]
+        private GameObject MixedRealityPlayspace;
 
+        [SerializeField]
+        [Tooltip("Drag & drop the Volume Camera Prefab here.")]
+        private GameObject VolumeCameraPrefab;
+        [SerializeField]
+        [Tooltip("Drag and drop the main camera gameobject here, to attach the MRTk MixedRealityInputModule and ConeCastGazeProvider components.")]
+        private GameObject MainCameraReference;
+
+        [SerializeField]
+        [Tooltip("Drag and drop the MRTk configuration profile onto this slot to ResetConfiguration once MRTk is instantiated.")]
+        private MixedRealityToolkitConfigurationProfile MRTKProfile;
+
+        private MixedRealityToolkit _mMRTk = null;
+
+        /// <summary>
+        /// Initialise the Volume Camera on VisionOS, and MRTk otherwise
+        /// </summary>
         internal void Initialization()
         {
 #if UNITY_VISIONOS || VISION_OS
+            Debug.LogInfo("On visionOS platform: no MRTk, adding VolumeCamera");
             initializeVolumeCamera();
-
-            mixedRealityToolkit.enabled = false;
-            mRTKHardwareRig.enabled = false;
-            mixedRealityInputModule.enabled = false;
-            gazeProvider.enabled = false;
-            Debug.Log("On vision os platform, MRTK scripts turned off");
 #else
-            Debug.Log("Not on visionos platform, no volume camera created");
-            mixedRealityToolkit.enabled = true;
-            mRTKHardwareRig.enabled = true;
-            mixedRealityInputModule.enabled = true;
-            gazeProvider.enabled = true;
+            Debug.LogInfo("Not on visionOS, no volume camera, adding MRTk");
+
+            _mMRTk ??= new GameObject("MixedRealityToolKit").AddComponent<MixedRealityToolkit>();
+            _mMRTk.ResetConfiguration(_mMRTkprofile);
+
+            MixedRealityPlayspace.AddComponent<MRTKHardwareRig>();
+            MainCameraReference.AddComponent<MixedRealityInputModule>();
+            MainCameraReference.AddComponent<ConeCastGazeProvider>();
 #endif
         }
 
+        /// <summary>
+        /// Instantiate the volume camera prefab
+        /// </summary>
         public void initializeVolumeCamera()
         {
-            Instantiate(volumeCamera, new Vector3(0,0,0), Quaternion.identity);
-            Debug.Log("Volume camera initialized");
+            Instantiate(VolumeCameraPrefab, new Vector3(0,0,0), Quaternion.identity);
+            Debug.LogInfo("[VolumeCameraManager] volume camera initialized.");
         }
     }
 }
