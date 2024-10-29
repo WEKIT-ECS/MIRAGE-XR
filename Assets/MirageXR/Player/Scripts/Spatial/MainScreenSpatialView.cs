10f2e7ba7106b0e70f1440195b686332c4ed0c72
Sergey Ilyasov <sergeyilyasov@gmail.com>
2024-10-29 01:09:28 +0100

    updated the sidebar and ornament

--- Assets/MirageXR/Player/Scripts/Spatial/MainScreenSpatialView.cs
+++ Assets/MirageXR/Player/Scripts/Spatial/MainScreenSpatialView.cs
@@ -10,23 +10,16 @@ namespace MirageXR
     public class MainScreenSpatialView : ScreenView
     {
         [Header("Buttons")]
-        [SerializeField] private Button _buttonSidebarCollapse;
-        [SerializeField] private Button _buttonSidebarExpand;
         [SerializeField] private Button _buttonSettings;
         [SerializeField] private Button _buttonSorting;
         [SerializeField] private Button _buttonAddNewActivity;
-        [Header("GameObjects")]
-        [SerializeField] private GameObject _sidebarOpened;
-        [SerializeField] private GameObject _sidebarClosed;
         [Header("InputField")]
         [SerializeField] private TMP_InputField _searchField;
         [Header("Container")]
         [SerializeField] private Transform _activityContainer;
         [Header("Prefabs")]
         [SerializeField] private ActivitySpatialListItem _activityListItemPrefab;
-
-        public void SetActionOnButtonSidebarCollapseClick(UnityAction action) => _buttonSidebarCollapse.SafeSetListener(action);
-        public void SetActionOnButtonSidebarExpandClick(UnityAction action) => _buttonSidebarExpand.SafeSetListener(action);
+        
         public void SetActionOnButtonSortingClick(UnityAction action) => _buttonSorting.SafeSetListener(action);
         public void SetActionOnButtonAddNewActivityClick(UnityAction action) => _buttonAddNewActivity.SafeSetListener(action);
         public void SetActionOnInputFieldSearchValueChanged(UnityAction<string> action) => _searchField.SafeSetListener(action);
