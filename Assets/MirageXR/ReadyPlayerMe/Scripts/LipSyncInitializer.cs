5fe75d637acda5d900fcd4ff84804ddc233c7799
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2024-12-18 20:56:22 +0100

    added documentation

--- Assets/MirageXR/ReadyPlayerMe/Scripts/LipSyncInitializer.cs
+++ Assets/MirageXR/ReadyPlayerMe/Scripts/LipSyncInitializer.cs
@@ -1,14 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using uLipSync;
 using UnityEngine;
 
 namespace MirageXR
 {
+	/// <summary>
+	/// Sets up the blend shape driver to prepare for the lip sync
+	/// Note that this does not add the uLipSync component as this is supposed to be added together with an audio source.
+	/// For the networked variant, this is done via a prefab
+	/// </summary>
 	public class LipSyncInitializer : AvatarInitializer
 	{
+		/// <summary>
+		/// Runs somewhere between the local setup and the networking setup
+		/// </summary>
 		public override int Priority => -2;
 
+		/// <summary>
+		/// Adds a uLipSyncBlendShape component as the blend shape driver for lip sync.
+		/// It then sets up the blend shape mapping.
+		/// </summary>
+		/// <param name="avatar">The avatar object which should be initialized</param>
 		public override void InitializeAvatar(GameObject avatar)
 		{
 			SkinnedMeshRenderer skinnedMeshRenderer = avatar.GetComponentInChildren<SkinnedMeshRenderer>();
