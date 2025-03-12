13c57594a37bfebafeb2ec04329d6a9a686fb72a
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2025-03-12 20:44:01 +0100

    connected animations to ReadyPlayerMe character

--- Assets/MirageXR/ContentTypes/VirtualInstructor/VirtualInstructorRPMInitializer.cs
+++ Assets/MirageXR/ContentTypes/VirtualInstructor/VirtualInstructorRPMInitializer.cs
@@ -1,6 +1,7 @@
 using MirageXR.View;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace MirageXR
 {
@@ -9,22 +10,35 @@ namespace MirageXR
 		// run this last
 		public override int Priority => -100;
 
-		public override void InitializeAvatar(GameObject avatar)
+		public override async void InitializeAvatar(GameObject avatar)
 		{
 			AvatarReferences avatarReferences = GetComponent<AvatarReferences>();
 			// disable IK
-			//avatarReferences.Rig.IK.RigLayer.weight = 0;
-			//avatarReferences.Rig.IK.HeadConstraint.weight = 0;
-			//avatarReferences.Rig.IK.HipsConstraint.weight = 0;
-			//avatarReferences.Rig.IK.HipsTarget.GetComponent<BodyController>().enabled = false;
-			//for (int i = 0; i < 2; i++)
-			//{
-			//	avatarReferences.Rig.IK.GetSide(i).Hand.Constraint.weight = 0;
-			//	avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandController>().enabled = false;
-			//	avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandJointsController>().enabled = false;
-			//	avatarReferences.Rig.IK.GetSide(i).Foot.Constraint.weight = 0;
-			//	avatarReferences.Rig.IK.GetSide(i).Foot.Target.GetComponent<FootController>().enabled = false;
-			//}
+			avatarReferences.Rig.IK.RigLayer.weight = 0;
+			avatarReferences.Rig.IK.HeadConstraint.weight = 0;
+			avatarReferences.Rig.IK.HipsConstraint.weight = 0;
+			avatarReferences.Rig.IK.HipsTarget.GetComponent<BodyController>().enabled = false;
+			for (int i = 0; i < 2; i++)
+			{
+				avatarReferences.Rig.IK.GetSide(i).Hand.Constraint.weight = 0;
+				avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandController>().enabled = false;
+				avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandJointsController>().enabled = false;
+				avatarReferences.Rig.IK.GetSide(i).Foot.Constraint.weight = 0;
+				avatarReferences.Rig.IK.GetSide(i).Foot.Target.GetComponent<FootController>().enabled = false;
+			}
+			Animator animator = avatar.GetComponent<Animator>();
+			string animatorControllerPath = "ReadyPlayerMe/AnimatorController";
+			var handle = Addressables.LoadAssetAsync<RuntimeAnimatorController>(animatorControllerPath);
+			await handle.Task;
+			if (handle.Status == AsyncOperationStatus.Succeeded)
+			{
+				Debug.Log("Runtime animator controller updated");
+				animator.runtimeAnimatorController = handle.Result;
+			}
+			else
+			{
+				Debug.LogError("Something went wrong loading the animator controller");
+			}
 		}
 	}
 }
