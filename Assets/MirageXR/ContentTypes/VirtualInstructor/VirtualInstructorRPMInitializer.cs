85a0286cf6c3b433e47c7e028d92eae0c7df99aa
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2025-03-12 18:49:47 +0100

    tweaked placement of ready player me character

--- Assets/MirageXR/ContentTypes/VirtualInstructor/VirtualInstructorRPMInitializer.cs
+++ Assets/MirageXR/ContentTypes/VirtualInstructor/VirtualInstructorRPMInitializer.cs
@@ -13,20 +13,18 @@ namespace MirageXR
 		{
 			AvatarReferences avatarReferences = GetComponent<AvatarReferences>();
 			// disable IK
-			avatarReferences.Rig.IK.RigLayer.weight = 0;
-			avatarReferences.Rig.IK.HeadConstraint.weight = 0;
-			avatarReferences.Rig.IK.HipsConstraint.weight = 0;
-			avatarReferences.Rig.IK.HipsTarget.GetComponent<BodyController>().enabled = false;
-			for (int i = 0; i < 2; i++)
-			{
-				avatarReferences.Rig.IK.GetSide(i).Hand.Constraint.weight = 0;
-				avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandController>().enabled = false;
-				avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandJointsController>().enabled = false;
-				avatarReferences.Rig.IK.GetSide(i).Foot.Constraint.weight = 0;
-				avatarReferences.Rig.IK.GetSide(i).Foot.Target.GetComponent<FootController>().enabled = false;
-			}
-
-			avatar.AddComponent<Instructor>();
+			//avatarReferences.Rig.IK.RigLayer.weight = 0;
+			//avatarReferences.Rig.IK.HeadConstraint.weight = 0;
+			//avatarReferences.Rig.IK.HipsConstraint.weight = 0;
+			//avatarReferences.Rig.IK.HipsTarget.GetComponent<BodyController>().enabled = false;
+			//for (int i = 0; i < 2; i++)
+			//{
+			//	avatarReferences.Rig.IK.GetSide(i).Hand.Constraint.weight = 0;
+			//	avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandController>().enabled = false;
+			//	avatarReferences.Rig.IK.GetSide(i).Hand.Target.GetComponent<HandJointsController>().enabled = false;
+			//	avatarReferences.Rig.IK.GetSide(i).Foot.Constraint.weight = 0;
+			//	avatarReferences.Rig.IK.GetSide(i).Foot.Target.GetComponent<FootController>().enabled = false;
+			//}
 		}
 	}
 }
