577a67170a74ea0df179efbcd3c14e04ceb2b5ed
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2025-04-10 15:58:34 +0200

    cleaned up avatar selection

--- Assets/MirageXR/Player/Scripts/Spatial/Popups/ChangeUserAvatarView.cs
+++ Assets/MirageXR/Player/Scripts/Spatial/Popups/ChangeUserAvatarView.cs
@@ -13,7 +13,6 @@ namespace MirageXR
 	public class ChangeUserAvatarView : PopupBase
 	{
 		[Header("References")]
-		[SerializeField] private RectTransform _confirmationPanel;
 		[SerializeField] private Button _closeWindowBtn;
 		[SerializeField] private Button _openEditorBtn;
 		[SerializeField] private CharacterModelSelectionElement _characterModelSelectionElement;
@@ -37,10 +36,7 @@ namespace MirageXR
 
 			_closeWindowBtn.onClick.AddListener(Close);
 			_openEditorBtn.onClick.AddListener(OpenEditor);
-			//_applyAvatarBtn.onClick.AddListener(ApplyAvatarUrl);
 			_characterModelSelectionElement.CharacterModelSelectionStarted += OpenAvatarLibrary;
-
-			//_avatarUrlField.text = UserSettings.AvatarUrl;
 
 			// add the current avatar to the library so that it can be loaded
 			if (!RootObject.Instance.AvatarLibraryManager.ContainsAvatar(UserSettings.AvatarUrl))
@@ -59,6 +55,7 @@ namespace MirageXR
 			UserSettings.AvatarUrlChanged -= OnAvatarUrlChanged;
 		}
 
+		// opens the avatar library menu (creates the menu instance if it did not already exist)
 		private void OpenAvatarLibrary()
 		{
 			if (_avatarLibraryMenuInstance == null)
@@ -67,6 +64,7 @@ namespace MirageXR
 				_avatarLibraryMenuInstance.CharacterModelSelected += OnAvatarSelected;
 			}
 			RectTransform avatarLibraryMenuRectTransform = _avatarLibraryMenuInstance.GetComponent<RectTransform>();
+			// place next to this menu
 			avatarLibraryMenuRectTransform.localPosition = new Vector3(
 				_rectTransform.sizeDelta.x + avatarLibraryMenuRectTransform.sizeDelta.x / 2f + 20,
 				0,
@@ -74,40 +72,30 @@ namespace MirageXR
 			_avatarLibraryMenuInstance.gameObject.SetActive(true);
 		}
 
+		// called if the user selected an avatar from the library view
+		// applies the selected avatar to the user settings
+		// this automatically invokes an event that the avatar was changed and this will update the UI
 		private void OnAvatarSelected(string modelUrl)
 		{
 			UserSettings.AvatarUrl = modelUrl;
 		}
 
+		// called if a new avatar was set in the user settings
 		private void OnAvatarUrlChanged(string newAvatarUrl)
 		{
-			//_avatarUrlField.text = UserSettings.AvatarUrl;
-			//StartCoroutine(ShowConfirmationPanel());
 			ApplyAvatarUrl();
 		}
 
+		// opens the ReadyPlayerMe editor website
 		private void OpenEditor()
 		{
 			Application.OpenURL(UserSettings.READYPLAYERME_EDITOR_URL);
 		}
 
+		// apply the currently saved AvatarUrl to the UI to reflect the selected avatar
 		private void ApplyAvatarUrl()
 		{
-			//if (!string.IsNullOrEmpty(_avatarUrlField.text))
-			//{
-			//	UserSettings.AvatarUrl = _avatarUrlField.text;
-			//	Debug.LogDebug("Changed avatar url to " + UserSettings.AvatarUrl);
-
-			//	StartCoroutine(ShowConfirmationPanel());
-			//}
 			_characterModelSelectionElement.Thumbnail.CharacterModelUrl = UserSettings.AvatarUrl;
-		}
-
-		private IEnumerator ShowConfirmationPanel()
-		{
-			_confirmationPanel.gameObject.SetActive(true);
-			yield return new WaitForSeconds(3f);
-			_confirmationPanel.gameObject.SetActive(false);
 		}
 	}
 }
