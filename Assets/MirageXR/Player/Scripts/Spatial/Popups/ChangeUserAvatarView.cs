904d6b8ab4f2df260c54ce5f870254be06ff2da4
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2025-04-10 15:42:53 +0200

    made avatar library menu reusable and included it in avatar selection screen

--- Assets/MirageXR/Player/Scripts/Spatial/Popups/ChangeUserAvatarView.cs
+++ Assets/MirageXR/Player/Scripts/Spatial/Popups/ChangeUserAvatarView.cs
@@ -16,8 +16,13 @@ namespace MirageXR
 		[SerializeField] private RectTransform _confirmationPanel;
 		[SerializeField] private Button _closeWindowBtn;
 		[SerializeField] private Button _openEditorBtn;
-		[SerializeField] private Button _applyAvatarBtn;
-		[SerializeField] private TMP_InputField _avatarUrlField;
+		[SerializeField] private CharacterModelSelectionElement _characterModelSelectionElement;
+
+		[Header("Prefabs")]
+		[SerializeField] private GameObject _avatarLibraryMenu;
+
+		private ReplaceModel _avatarLibraryMenuInstance;
+		private RectTransform _rectTransform;
 
 		protected override bool TryToGetArguments(params object[] args)
 		{
@@ -27,24 +32,58 @@ namespace MirageXR
 		public override void Initialization(Action<PopupBase> onClose, params object[] args)
 		{
 			base.Initialization(onClose, args);
+			
+			_rectTransform = GetComponent<RectTransform>();
 
 			_closeWindowBtn.onClick.AddListener(Close);
 			_openEditorBtn.onClick.AddListener(OpenEditor);
-			_applyAvatarBtn.onClick.AddListener(ApplyAvatarUrl);
+			//_applyAvatarBtn.onClick.AddListener(ApplyAvatarUrl);
+			_characterModelSelectionElement.CharacterModelSelectionStarted += OpenAvatarLibrary;
+
+			//_avatarUrlField.text = UserSettings.AvatarUrl;
+
+			// add the current avatar to the library so that it can be loaded
+			if (!RootObject.Instance.AvatarLibraryManager.ContainsAvatar(UserSettings.AvatarUrl))
+			{
+				RootObject.Instance.AvatarLibraryManager.AddAvatar(UserSettings.AvatarUrl);
+			}
+
 			UserSettings.AvatarUrlChanged += OnAvatarUrlChanged;
 
-			_avatarUrlField.text = UserSettings.AvatarUrl;
+			ApplyAvatarUrl();
 		}
 
 		private void OnDestroy()
 		{
+			_characterModelSelectionElement.CharacterModelSelectionStarted -= OpenAvatarLibrary;
 			UserSettings.AvatarUrlChanged -= OnAvatarUrlChanged;
+		}
+
+		private void OpenAvatarLibrary()
+		{
+			if (_avatarLibraryMenuInstance == null)
+			{
+				_avatarLibraryMenuInstance = Instantiate(_avatarLibraryMenu, transform).GetComponent<ReplaceModel>();
+				_avatarLibraryMenuInstance.CharacterModelSelected += OnAvatarSelected;
+			}
+			RectTransform avatarLibraryMenuRectTransform = _avatarLibraryMenuInstance.GetComponent<RectTransform>();
+			avatarLibraryMenuRectTransform.localPosition = new Vector3(
+				_rectTransform.sizeDelta.x + avatarLibraryMenuRectTransform.sizeDelta.x / 2f + 20,
+				0,
+				0);
+			_avatarLibraryMenuInstance.gameObject.SetActive(true);
+		}
+
+		private void OnAvatarSelected(string modelUrl)
+		{
+			UserSettings.AvatarUrl = modelUrl;
 		}
 
 		private void OnAvatarUrlChanged(string newAvatarUrl)
 		{
-			_avatarUrlField.text = UserSettings.AvatarUrl;
-			StartCoroutine(ShowConfirmationPanel());
+			//_avatarUrlField.text = UserSettings.AvatarUrl;
+			//StartCoroutine(ShowConfirmationPanel());
+			ApplyAvatarUrl();
 		}
 
 		private void OpenEditor()
@@ -54,13 +93,14 @@ namespace MirageXR
 
 		private void ApplyAvatarUrl()
 		{
-			if (!string.IsNullOrEmpty(_avatarUrlField.text))
-			{
-				UserSettings.AvatarUrl = _avatarUrlField.text;
-				Debug.LogDebug("Changed avatar url to " + UserSettings.AvatarUrl);
+			//if (!string.IsNullOrEmpty(_avatarUrlField.text))
+			//{
+			//	UserSettings.AvatarUrl = _avatarUrlField.text;
+			//	Debug.LogDebug("Changed avatar url to " + UserSettings.AvatarUrl);
 
-				StartCoroutine(ShowConfirmationPanel());
-			}
+			//	StartCoroutine(ShowConfirmationPanel());
+			//}
+			_characterModelSelectionElement.Thumbnail.CharacterModelUrl = UserSettings.AvatarUrl;
 		}
 
 		private IEnumerator ShowConfirmationPanel()
