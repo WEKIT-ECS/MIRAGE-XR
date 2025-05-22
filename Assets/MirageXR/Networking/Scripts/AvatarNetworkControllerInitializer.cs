65173bf7f6c55c95ce58764566426647c79d6ca8
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2025-05-22 17:39:46 +0200

    added interface to make the virtual instructor talk, audio clips are now immediately spoken by the instructor

--- Assets/MirageXR/Networking/Scripts/AvatarNetworkControllerInitializer.cs
+++ Assets/MirageXR/Networking/Scripts/AvatarNetworkControllerInitializer.cs
@@ -50,7 +50,7 @@ namespace MirageXR
 			//uLipSyncBlendShape blendShapeController = avatar.GetComponent<uLipSyncBlendShape>();
 			//lipSync.onLipSyncUpdate.AddListener(blendShapeController.OnLipSyncUpdate);
 			//blendShapeController.maxBlendShapeValue = 1;
-			avatarRefs.OfflineReferences.Speaker = _voiceNetworkObject.GetComponent<AudioSource>();
+			avatarRefs.OfflineReferences.AudioSource = _voiceNetworkObject.GetComponent<AudioSource>();
 			_voiceNetworkObject.GetComponentInChildren<CollaborativeAudioSource>().BindLipSync();
 		}
 
