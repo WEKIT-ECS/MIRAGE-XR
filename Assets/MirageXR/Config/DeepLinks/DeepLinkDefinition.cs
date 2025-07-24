339d3cd92fe2c7fd3f8622bb634077cebc7a2c5e
Alex Shubin <icawke@gmail.com>
2025-07-24 16:19:35 +0300

    activity deep links have been updated

--- Assets/MirageXR/Config/DeepLinks/DeepLinkDefinition.cs
+++ Assets/MirageXR/Config/DeepLinks/DeepLinkDefinition.cs
@@ -1,9 +1,8 @@
 ﻿using i5.Toolkit.Core.DeepLinkAPI;
 using MirageXR;
 using System;
-using System.IO;
-using System.Threading.Tasks;
-using UnityEngine;
+using Cysharp.Threading.Tasks;
+using MemoryPack.Internal;
 
 /// <summary>
 /// Defines deep link paths to which the application should react
@@ -15,90 +14,71 @@ public class DeepLinkDefinition
     /// </summary>
     /// <param name="args">Arguments that are passed with the deep link;
     /// should contain the parameters id and download</param>
+    [Preserve]
     [DeepLink(path: "load")]
-    public async void LoadActivity(DeepLinkArgs args)
+    public void LoadActivity(DeepLinkArgs args)
     {
-        if (args.Parameters.TryGetValue("activity", out string activityId))
+        LoadActivityAsync(args).Forget();
+    }
+
+    private async UniTask LoadActivityAsync(DeepLinkArgs args)
+    {
+        await RootObject.Instance.LEE.WaitForInitialization();
+
+        if (args.Parameters.TryGetValue("activity", out var activityId))
         {
-            if (File.Exists(Path.Combine(Application.persistentDataPath, activityId)))
-            {
-                await Open(activityId);
-            }
-            else
-            {
-                if (args.Parameters.TryGetValue("download", out string downloadPath))
-                {
-                    bool success = await Download(downloadPath, activityId);
-                    if (success)
-                    {
-                        await Open(activityId);
-                    }
-                }
-            }
+            await Open(activityId);
         }
         else
         {
             Debug.LogError("Deep Link is missing 'activity' guid parameter");
-            DialogWindow.Instance.Show(
-            "Info!",
-            "Activity launch failed, parameter 'activity' is missing.",
-            new DialogButtonContent("Ok"));
+            DialogWindow.Instance.Show("Info!", "Activity launch failed, parameter 'activity' is missing.", new DialogButtonContent("Ok"));
         }
     }
 
     /// <summary>
     /// Creates a new activity via a deep link, e.g. using wekit:/new
     /// </summary>
+    [Preserve]
     [DeepLink(path: "new")]
-    public async void NewActivity()
+    public void NewActivity()
     {
-        await RootObject.Instance.EditorSceneService.LoadEditorAsync();
-        await LearningExperienceEngine.LearningExperienceEngine.Instance.ActivityManagerOld.CreateNewActivity();
+        NewActivityAsync().Forget();
+    }
+
+    private async UniTask NewActivityAsync()
+    {
+        await RootObject.Instance.LEE.WaitForInitialization();
+
+        var baseCamera = RootObject.Instance.BaseCamera;
+        var position = (baseCamera.transform.forward * 0.5f) + baseCamera.transform.position;   //TODO: move to Manager
+        RootObject.Instance.LEE.ActivityManager.CreateNewActivity(position);
     }
 
     // opens the given activity
-    private async Task Open(string activity)
+    private async UniTask Open(string activityID)
     {
-        string fullActivityJson = $"{activity}-activity.json";
-        PlayerPrefs.SetString("activityUrl", fullActivityJson);
-        PlayerPrefs.Save();
-
-        await RootObject.Instance.EditorSceneService.LoadEditorAsync();
-        await LearningExperienceEngine.LearningExperienceEngine.Instance.ActivityManagerOld.LoadActivity(fullActivityJson);
-    }
-
-    // downloads the zip file of the activity from the given downloadPath and unzips the file
-    private async Task<bool> Download(string downloadPath, string activityId)
-    {
-        Debug.Log("Downloading session");
-
-        bool success;
-        using (LearningExperienceEngine.SessionDownloader downloader = new LearningExperienceEngine.SessionDownloader(
-            LearningExperienceEngine.UserSettings.domain + "/pluginfile.php/" + downloadPath,
-            activityId + ".zip"))
+        if (Guid.TryParse(activityID, out var guid))
         {
-            success = await downloader.DownloadZipFileAsync();
-
-            if (success)
+            try
             {
-                try
+                var loadView = LoadView.Instance;
+                if (loadView)
                 {
-                    await downloader.UnzipFileAsync();
+                    loadView.Show();
                 }
-                catch (Exception e)
+                //TODO: add ActivityLoadStartedEvent
+                await RootObject.Instance.LEE.ActivityManager.LoadActivityAsync(guid);
+                //TODO: add ActivityLoadStartedEvent
+                if (loadView)
                 {
-                    Debug.LogError(e.ToString());
-                    success = false;
+                    loadView.Hide();
                 }
             }
-            else
+            catch (Exception e)
             {
-                DialogWindow.Instance.Show(
-                "Info!",
-                "Activity download link not valid! Please check that you are connected to the correct Moodle repository",
-                new DialogButtonContent("Ok"));
+                UnityEngine.Debug.LogError(e);
             }
         }
-        return success;
     }
 }
