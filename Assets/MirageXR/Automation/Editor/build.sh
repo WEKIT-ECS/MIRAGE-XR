36a8ebad5e8c15a9ddde49db540d5ef97c977438
Fridolin Wild <fridolin.wild@gmail.com>
2026-03-06 10:56:40 +0000

    asmdef fixes for CIhelpers, shell command update for build automation

--- Assets/MirageXR/Automation/Editor/build.sh
+++ Assets/MirageXR/Automation/Editor/build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
-/Applications/Unity/Hub/Editor/2022.3.7f1/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Hub/Editor/6000.0.59f2/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile - \
 	-projectPath "$(pwd)" \
 	-quit \
-	-executeMethod LocalBuildPipeline.BuildQuest
+        -executeMethod BuildCommand.Build	
+# -executeMethod LocalBuildPipeline.BuildQuest
 
 echo "done."
