3a52208481bb6c5272d994ed27da8dc1fffa2ced
Herman Saetre <hermansaetre1412@gmail.com>
2026-07-11 19:50:17 +0200

    fix more hover on interactable elements

--- Assets/MirageXR/Utility.UiKit/Prefabs/UI_KIT_Spatial/Toggles/SwitchToggleImageAndText_Spatial.cs
+++ Assets/MirageXR/Utility.UiKit/Prefabs/UI_KIT_Spatial/Toggles/SwitchToggleImageAndText_Spatial.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,10 +6,15 @@ namespace MirageXR
 {
     public class SwitchToggleImageAndText_Spatial : MonoBehaviour
     {
+        private const float ActiveAlpha = 1f;
+        private const float InactiveAlpha = 0f;
+
         [SerializeField] protected Toggle _toggle;
         [SerializeField] private GameObject _handleImage;
         [SerializeField] private GameObject _textOn;
         [SerializeField] private GameObject _textOff;
+
+        private Coroutine _refreshRoutine;
 
         protected virtual void Start()
         {
@@ -20,14 +26,57 @@ namespace MirageXR
         {
             if (_handleImage != null)
             {
-                _handleImage.SetActive(value);
+                UpdateToggleColors(value ? 1f : 0f);
+                RefreshAfterDeselect(value);
             }
             _textOff?.SetActive(!value);
             _textOn?.SetActive(value);
         }
 
+        private void UpdateToggleColors(float alpha)
+        {
+            var colors = _toggle.colors;
+            var normalColor = colors.normalColor;
+            normalColor.a = alpha;
+            colors.normalColor = normalColor;
+            _toggle.colors = colors;
+        }
+
+        private void RefreshAfterDeselect(bool value)
+        {
+            if (value)
+            {
+                return;
+            }
+
+            if (_refreshRoutine != null)
+            {
+                StopCoroutine(_refreshRoutine);
+            }
+            _refreshRoutine = StartCoroutine(RefreshAfterDeselectRoutine());
+        }
+
+        private IEnumerator RefreshAfterDeselectRoutine() //The first selected toggle has no hover effect unless it is refreshed
+        {
+            yield return null;
+            RefreshParentGameObject();
+            _refreshRoutine = null;
+        }
+
+        private void RefreshParentGameObject()
+        {
+            var parent = _toggle.transform.parent;
+            parent.gameObject.SetActive(false);
+            parent.gameObject.SetActive(true);
+        }
+
         protected virtual void OnDestroy()
         {
+            if (_refreshRoutine != null)
+            {
+                StopCoroutine(_refreshRoutine);
+            }
+
             _toggle.onValueChanged.RemoveListener(UpdateView);
         }
     }
